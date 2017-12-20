488ae756373215c8b17c75b8e38409e7f32d7709
Max Kanter <kmax12@gmail.com>
2017-12-19 23:34:55 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -26,10 +26,10 @@ setup(
     classifiers=[
          'Development Status :: 3 - Alpha',
          'Intended Audience :: Developers',
-         'Programming Language :: Python :: 2.7'
-         'Programming Language :: Python :: 3'
-         'Programming Language :: Python :: 3.4'
-         'Programming Language :: Python :: 3.5'
+         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.4',
+         'Programming Language :: Python :: 3.5',
          'Programming Language :: Python :: 3.6'
     ],
     install_requires=open('requirements.txt').readlines(),
