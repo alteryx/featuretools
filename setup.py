6280362daa42d904da72907e63e4a327941238f4
Benjamin Schreck <benjaminschreck93@gmail.com>
2018-01-12 11:46:50 -0700

    update version tag in ft itself

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ class build_ext(_build_ext):
 
 setup(
     name='featuretools',
-    version='0.1.16',
+    version='0.1.16.d3m',
     packages=find_packages(),
     package_data={'featuretools': ['config_yaml.txt']},
     description='a framework for automated feature engineering',
