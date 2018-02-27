d46b0682d244ac288dfd3e6d67754168d5ed2ae1
Seth-Rothschild <seth.rothschild@featurelabs.com>
2018-02-27 14:52:54 -0500

    Change entity names

--- featuretools/demo/retail.py
+++ featuretools/demo/retail.py
@@ -29,8 +29,8 @@ def load_retail(id='demo_retail_data', nrows=None):
             Out[3]:
             Entityset: demo_retail_data
               Entities:
-                invoices (shape = [25900, 3])
-                items (shape = [4070, 3])
+                orders (shape = [25900, 3])
+                products (shape = [4070, 3])
                 customers (shape = [4373, 3])
                 transactions (shape = [541909, 6])
 
@@ -45,8 +45,8 @@ def load_retail(id='demo_retail_data', nrows=None):
             Out[3]:
             Entityset: demo_retail_data
               Entities:
-                invoices (shape = [66, 3])
-                items (shape = [590, 3])
+                orders (shape = [66, 3])
+                products (shape = [590, 3])
                 customers (shape = [49, 3])
                 transactions (shape = [1000, 6])
 
@@ -65,35 +65,33 @@ def load_retail(id='demo_retail_data', nrows=None):
     df = pd.read_csv(demo_save_path,
                      nrows=nrows,
                      parse_dates=["InvoiceDate"])
-
     df.rename(columns={'Unnamed: 0': 'transaction_id',
-                       'InvoiceNo': 'invoice_id',
+                       'InvoiceNo': 'order_id',
                        'StockCode': 'product_id',
                        'Description': 'description',
                        'Quantity': 'quantity',
-                       'InvoiceDate': 'invoice_date',
+                       'InvoiceDate': 'order_date',
                        'UnitPrice': 'price',
                        'CustomerID': 'customer_id',
                        'Country': 'country'},
               inplace=True)
-
     es.entity_from_dataframe("transactions",
                              dataframe=df,
                              index="transaction_id",
-                             time_index="invoice_date")
+                             time_index="order_date")
 
-    es.normalize_entity(new_entity_id="items",
+    es.normalize_entity(new_entity_id="products",
                         base_entity_id="transactions",
                         index="product_id",
                         additional_variables=["description"])
 
-    es.normalize_entity(new_entity_id="invoices",
+    es.normalize_entity(new_entity_id="orders",
                         base_entity_id="transactions",
-                        index="invoice_id",
+                        index="order_id",
                         additional_variables=["customer_id", "country"])
 
     es.normalize_entity(new_entity_id="customers",
-                        base_entity_id="invoices",
+                        base_entity_id="orders",
                         index="customer_id",
                         additional_variables=["country"])
     es.add_last_time_indexes()
