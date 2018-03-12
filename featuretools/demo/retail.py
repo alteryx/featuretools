a68e1e7e5150ad685d366e41bd678ea6ad7beaef
Seth-Rothschild <seth.rothschild@featurelabs.com>
2018-03-12 17:29:54 -0400

    transactions -> order_products

--- featuretools/demo/retail.py
+++ featuretools/demo/retail.py
@@ -4,6 +4,7 @@ from builtins import str
 import pandas as pd
 
 import featuretools as ft
+import featuretools.variable_types as vtypes
 from featuretools.config import config as ft_config
 
 
@@ -32,7 +33,7 @@ def load_retail(id='demo_retail_data', nrows=None):
                 orders (shape = [25900, 3])
                 products (shape = [4070, 3])
                 customers (shape = [4373, 3])
-                transactions (shape = [541909, 6])
+                order_products (shape = [541909, 6])
 
         Load in subset of data
 
@@ -48,7 +49,7 @@ def load_retail(id='demo_retail_data', nrows=None):
                 orders (shape = [66, 3])
                 products (shape = [590, 3])
                 customers (shape = [49, 3])
-                transactions (shape = [1000, 6])
+                order_products (shape = [1000, 6])
 
     '''
     demo_save_path = make_retail_pathname(nrows)
@@ -60,7 +61,7 @@ def load_retail(id='demo_retail_data', nrows=None):
         df = pd.read_csv(csv_s3,
                          nrows=nrows,
                          parse_dates=["InvoiceDate"])
-        df.to_csv(demo_save_path, index_label='transaction_id')
+        df.to_csv(demo_save_path, index_label='order_product_id')
 
     df = pd.read_csv(demo_save_path,
                      nrows=nrows,
@@ -75,20 +76,22 @@ def load_retail(id='demo_retail_data', nrows=None):
                        'CustomerID': 'customer_id',
                        'Country': 'country'},
               inplace=True)
-    es.entity_from_dataframe("transactions",
+    es.entity_from_dataframe("order_products",
                              dataframe=df,
-                             index="transaction_id",
-                             time_index="order_date")
+                             index="order_product_id",
+                             variable_types={'description': vtypes.Text})
 
     es.normalize_entity(new_entity_id="products",
-                        base_entity_id="transactions",
+                        base_entity_id="order_products",
                         index="product_id",
                         additional_variables=["description"])
 
     es.normalize_entity(new_entity_id="orders",
-                        base_entity_id="transactions",
+                        base_entity_id="order_products",
                         index="order_id",
-                        additional_variables=["customer_id", "country"])
+                        additional_variables=[
+                            "customer_id", "country", "order_date"],
+                        make_time_index="order_date")
 
     es.normalize_entity(new_entity_id="customers",
                         base_entity_id="orders",
