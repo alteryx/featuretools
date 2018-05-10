8abf26d73f31a89250e7451a8ce34de2e3b017c4
bschreck <benjaminschreck93@gmail.com>
2018-05-10 08:51:04 -0700

    remove unused utility functions

--- featuretools/utils/wrangle.py
+++ featuretools/utils/wrangle.py
@@ -9,39 +9,6 @@ from featuretools import variable_types
 from featuretools.entityset.timedelta import Timedelta
 
 
-def flatten_2d(array):
-    """
-    Converts numpy array to a 2-dimensional matrix.
-    If an array has any list-like elements, they will be expanded into their own columns
-
-    Example:
-    x = np.array([[np.array([1,2]), 2],
-                  [np.array([3,4]), 4]])
-    flattened = flatten_2d(x)
-    flattened
-    >>> np.array([[1, 2, 2],
-                  [3, 4, 4]])
-    """
-    nonscalars = {c: len(v)
-                  for c, v in enumerate(array[0, :])
-                  if not np.isscalar(v)}
-    new_width = array.shape[1] + sum([v - 1 for v in nonscalars.values()])
-    new_matrix = np.zeros((array.shape[0], new_width))
-    i = 0
-    c = 0
-    while i < new_width:
-        if c in nonscalars:
-            c_len = nonscalars[c]
-            values = np.concatenate(array[:, c]).reshape(array.shape[0], c_len)
-            new_matrix[:, i: i + c_len] = values
-            i += c_len
-        else:
-            new_matrix[:, i] = array[:, c]
-            i += 1
-        c += 1
-    return new_matrix
-
-
 def _check_timedelta(td, entity_id=None, related_entity_id=None):
     """
     Convert strings to Timedelta objects
@@ -182,35 +149,6 @@ def _check_time_against_column(time, time_column):
         return (isinstance(time_column, (variable_types.Datetime, variable_types.DatetimeTimeIndex)) or
                 (isinstance(time_column, (variable_types.Ordinal, variable_types.Numeric, variable_types.TimeIndex)) and
                  time.unit not in Timedelta._time_units))
-    else:
-        return False
-
-
-def _check_time_against_time(time1, time2):
-    '''
-    Check to make sure that time1 is compatible with time2,
-    where time1 could be a timestamp, timedelta, number, or None,
-    and time2 could be a number or Timedelta. Compatibility means that
-    arithmetic can be performed between time1 and time2.
-
-    If time1 is None, then we don't care if arithmetic can be performed
-    (presumably it won't ever be performed)
-    '''
-    if time1 is None:
-        return True
-    elif isinstance(time1, (int, float)):
-        return isinstance(time2, (int, float))
-    elif isinstance(time1, Timedelta):
-        if isinstance(time2, (pd.Timestamp, datetime)):
-            return True
-        elif time1.unit == Timedelta._Observations:
-            return True
-        elif time1.unit == Timedelta._generic_unit:
-            return True
-        else:
-            return False
-    elif isinstance(time1, (pd.Timestamp, datetime)):
-        return isinstance(time2, (pd.Timedelta))
     else:
         return False
 
