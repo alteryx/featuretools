f6b193c311658fe69c52013a7d336bc6f8ae803d
bschreck <benjaminschreck93@gmail.com>
2018-06-04 10:25:26 -0700

    switched order of build_featuretools.sh

--- conda.recipe/build_featuretools.sh
+++ conda.recipe/build_featuretools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /featuretools
-conda build .
 yes | anaconda login --username=$1 --password=$2
+conda build .
 for build_v in $(conda build . --output); do anaconda upload --user featuretools $build_v; done
