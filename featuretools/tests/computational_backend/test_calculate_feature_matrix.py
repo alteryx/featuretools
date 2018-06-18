ff3ab53c2fb7d6fc1af92dffd8bf332a3562e360
Roy Wedge <roy.wedge@gmail.com>
2018-06-18 15:30:42 -0400

    test error within parallel try/except is properly raised

--- featuretools/tests/computational_backend/test_calculate_feature_matrix.py
+++ featuretools/tests/computational_backend/test_calculate_feature_matrix.py
@@ -837,6 +837,26 @@ def test_dask_persisted_entityset(entityset, capsys):
         assert (feature_matrix == labels).values.all()
 
 
+def test_parallel_failure_raises_correct_error(entityset):
+    times = list([datetime(2011, 4, 9, 10, 30, i * 6) for i in range(5)] +
+                 [datetime(2011, 4, 9, 10, 31, i * 9) for i in range(4)] +
+                 [datetime(2011, 4, 9, 10, 40, 0)] +
+                 [datetime(2011, 4, 10, 10, 40, i) for i in range(2)] +
+                 [datetime(2011, 4, 10, 10, 41, i * 3) for i in range(3)] +
+                 [datetime(2011, 4, 10, 11, 10, i * 3) for i in range(2)])
+    cutoff_time = pd.DataFrame({'time': times, 'instance_id': range(17)})
+    property_feature = IdentityFeature(entityset['log']['value']) > 10
+
+    with pytest.raises(AssertionError):
+        calculate_feature_matrix([property_feature],
+                                 entityset=entityset,
+                                 cutoff_time=cutoff_time,
+                                 verbose=True,
+                                 chunk_size=.13,
+                                 njobs=0,
+                                 approximate='1 hour')
+
+
 def test_njobs(entityset):
     if version_info.major == 2:
         import multiprocessing
