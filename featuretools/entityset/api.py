e7a1ce0ff36a6708b8a78c7faf52f2e89db09d71
bschreck <benjaminschreck93@gmail.com>
2018-06-21 13:56:35 -0700

    remove metadata_filename arg

--- featuretools/entityset/api.py
+++ featuretools/entityset/api.py
@@ -2,5 +2,5 @@
 from .entity import Entity
 from .entityset import EntitySet
 from .relationship import Relationship
-from .serialization import read_parquet, read_pickle
+from .serialization import read_parquet, read_pickle, read_entityset
 from .timedelta import Timedelta
