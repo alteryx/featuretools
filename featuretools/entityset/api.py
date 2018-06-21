ec64175acc35a03d58d2ef8fd16cf0d33df57a11
bschreck <benjaminschreck93@gmail.com>
2018-06-21 16:36:29 -0700

    linted

--- featuretools/entityset/api.py
+++ featuretools/entityset/api.py
@@ -2,5 +2,5 @@
 from .entity import Entity
 from .entityset import EntitySet
 from .relationship import Relationship
-from .serialization import read_parquet, read_pickle, read_entityset
+from .serialization import read_entityset, read_parquet, read_pickle
 from .timedelta import Timedelta
