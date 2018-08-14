f4f30e3faf785461ea004b68a10e33881fe221be
Seth Rothschild <seth.j.rothschild@gmail.com>
2018-08-14 13:58:44 -0400

    Use filenames from S3 for Demos (#207)
    * use s3 csv names
    
    * add variables for csv names
    
    * sort imports with isort

--- featuretools/tests/demo_tests/test_demo_data.py
+++ featuretools/tests/demo_tests/test_demo_data.py
@@ -2,30 +2,30 @@ import os
 
 from featuretools.demo import load_flight, load_mock_customer, load_retail
 from featuretools.demo.flight import make_flight_pathname
-from featuretools.demo.retail import make_retail_pathname
+from featuretools.demo.retail import RETAIL_CSV, make_retail_pathname
 from featuretools.synthesis import dfs
 
 
 def test_load_retail_save():
     nrows = 10
+
     load_retail(nrows=nrows, return_single_table=True)
-    assert os.path.isfile(make_retail_pathname(nrows))
-    assert os.path.getsize(make_retail_pathname(nrows)) < 45580670
-    os.remove(make_retail_pathname(nrows))
+    assert os.path.isfile(make_retail_pathname(nrows, RETAIL_CSV))
+    assert os.path.getsize(make_retail_pathname(nrows, RETAIL_CSV)) < 45580670
+    os.remove(make_retail_pathname(nrows, RETAIL_CSV))
 
 
 def test_load_retail_diff():
     nrows = 10
     es_first = load_retail(nrows=nrows)
-    assert os.path.isfile(make_retail_pathname(nrows))
     assert es_first['order_products'].df.shape[0] == nrows
 
     nrows_second = 11
     es_second = load_retail(nrows=nrows_second)
-    assert os.path.isfile(make_retail_pathname(nrows_second))
     assert es_second['order_products'].df.shape[0] == nrows_second
-    os.remove(make_retail_pathname(nrows))
-    os.remove(make_retail_pathname(nrows_second))
+
+    os.remove(make_retail_pathname(nrows, RETAIL_CSV))
+    os.remove(make_retail_pathname(nrows_second, RETAIL_CSV))
 
 
 def test_mock_customer():
