e346e8e6f7ed4619050975be5c76ebba9bb33eb3
Seth Rothschild <seth.j.rothschild@gmail.com>
2018-08-31 23:20:43 -0400

    check for multiple inputs (use code from #86)

--- featuretools/synthesis/deep_feature_synthesis.py
+++ featuretools/synthesis/deep_feature_synthesis.py
@@ -690,8 +690,9 @@ def check_stacking(primitive, input_types):
             if type(f) in primitive.stack_on_exclude:
                 return False
 
-    if primitive.expanding:
-        return False
+    for f in input_types:
+        if f.expanding:
+            return False
 
     for f in input_types:
         if f.base_of_exclude is not None:
