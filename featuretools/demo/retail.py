ad1250d2702f3bcdeee06a2f681282626fcbb10e
Roy Wedge <roy.wedge@gmail.com>
2018-09-14 12:12:15 -0400

    no longer cache demo entitysets

--- featuretools/demo/retail.py
+++ featuretools/demo/retail.py
@@ -1,14 +1,10 @@
-import os
-from builtins import str
-
 import pandas as pd
 
 import featuretools as ft
 import featuretools.variable_types as vtypes
-from featuretools.config import config as ft_config
 
 
-def load_retail(id='demo_retail_data', nrows=None, return_single_table=False, use_cache=True):
+def load_retail(id='demo_retail_data', nrows=None, return_single_table=False):
     '''
     Returns the retail entityset example which is a modified version of
     the data found `here <https://archive.ics.uci.edu/ml/datasets/online+retail>`_.
@@ -18,8 +14,6 @@ def load_retail(id='demo_retail_data', nrows=None, return_single_table=False, us
         nrows (int):  Number of rows to load of the underlying CSV.
             If None, load all.
         return_single_table (bool): If True, return a CSV rather than an EntitySet. Default is False.
-        use_cache (bool): If True, use a stored version of the CSV. Otherwise redownload.
-            Default is True.
 
     Examples:
 
@@ -58,16 +52,8 @@ def load_retail(id='demo_retail_data', nrows=None, return_single_table=False, us
     '''
     es = ft.EntitySet(id)
     csv_s3 = "https://s3.amazonaws.com/featuretools-static/" + RETAIL_CSV + ".csv"
-    demo_save_path = make_retail_pathname(nrows, RETAIL_CSV)
 
-    if not use_cache or not os.path.isfile(demo_save_path):
-
-        df = pd.read_csv(csv_s3,
-                         nrows=nrows,
-                         parse_dates=["order_date"])
-        df.to_csv(demo_save_path, index_label='order_product_id')
-
-    df = pd.read_csv(demo_save_path,
+    df = pd.read_csv(csv_s3,
                      nrows=nrows,
                      parse_dates=["order_date"])
 
@@ -96,11 +82,5 @@ def load_retail(id='demo_retail_data', nrows=None, return_single_table=False, us
     es.add_last_time_indexes()
 
     return es
-
-
-def make_retail_pathname(nrows, csv_name):
-    file_name = csv_name + '_' + str(nrows) + '.csv'
-    return os.path.join(ft_config['csv_save_location'], file_name)
-
 
 RETAIL_CSV = "online-retail-logs-2018-08-28"
