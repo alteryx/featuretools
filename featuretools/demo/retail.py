68614b53a17cac766e99352c322c712906633952
Will Koehrsen <wjk68@case.edu>
2018-10-02 08:43:34 -0400

    Added fallback to csv
    Tries to load `gz` compressed and defaults to uncompressed `csv`  if error.

--- featuretools/demo/retail.py
+++ featuretools/demo/retail.py
@@ -59,12 +59,20 @@ def load_retail(id='demo_retail_data', nrows=None, return_single_table=False):
 
 '''
     es = ft.EntitySet(id)
-    csv_s3 = "https://s3.amazonaws.com/featuretools-static/" + RETAIL_CSV + ".csv.gz"
-
-    df = pd.read_csv(csv_s3,
-                     nrows=nrows,
-                     parse_dates=["order_date"])
-
+    csv_s3_gz = "https://s3.amazonaws.com/featuretools-static/" + RETAIL_CSV + ".csv.gz"
+    csv_s3 = "https://s3.amazonaws.com/featuretools-static/" + RETAIL_CSV + ".csv"
+    
+    # Try to read in gz compressed file
+    try:
+        df = pd.read_csv(csv_s3_gz,
+                         nrows=nrows,
+                         parse_dates=["order_date"])
+    # Fall back to uncompressed
+    except:
+        df = pd.read_csv(csv_s3,
+                         nrows=nrows,
+                         parse_dates=["order_date"])
+   
     if return_single_table:
         return df
 
