02b52d37ac8b984c4aae0352b89289358f57f276
Max Kanter <kmax12@gmail.com>
2018-10-25 16:46:50 -0400

    remove redirect std streams

--- featuretools/utils/gen_utils.py
+++ featuretools/utils/gen_utils.py
@@ -31,25 +31,6 @@ def session_type():
     return "ipython"
 
 
-class RedirectStdStreams(object):
-
-    def __init__(self, stdout=None, stderr=None):
-        self._stdout = stdout or sys.stdout
-        self._stderr = stderr or sys.stderr
-
-    def __enter__(self):
-        self.old_stdout, self.old_stderr = sys.stdout, sys.stderr
-        self.old_stdout.flush()
-        self.old_stderr.flush()
-        sys.stdout, sys.stderr = self._stdout, self._stderr
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self._stdout.flush()
-        self._stderr.flush()
-        sys.stdout = self.old_stdout
-        sys.stderr = self.old_stderr
-
-
 def make_tqdm_iterator(**kwargs):
     options = {
         "file": sys.stdout,
