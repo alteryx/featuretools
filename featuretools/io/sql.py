c82e7d7f8a0a8b8d384ce9b5031e2a4cb745fe39
bentona <benton.anderson@gmail.com>
2018-11-05 11:58:03 -0500

    wrap up remaining API options

--- featuretools/io/sql.py
+++ featuretools/io/sql.py
@@ -4,9 +4,15 @@ import sqlalchemy as sa
 import featuretools as ft
 
 
-def from_sql(id, connection):
-    loader = EntitySetLoaderFromSQL(connection=connection, id=id)
-    loader.load_all_tables()
+def from_sql(id, connection=None, connection_string=None, tables=None, **kwargs):
+    if connection is None:
+        if connection_string is None:
+            raise ValueError('connection or connection_string is required')
+        else:
+            connection = sa.create_engine(connection_string, echo=False).connect()
+
+    loader = EntitySetLoaderFromSQL(connection=connection, id=id, passthrough_args=kwargs)
+    loader.load_tables(tables)
     loader.load_relationships()
     return loader.es
 
@@ -23,15 +29,19 @@ def _find_pk_name(table):
 
 
 class EntitySetLoaderFromSQL:
-    def __init__(self, id, connection):
+    def __init__(self, id, connection=None, passthrough_args={}):
         self.connection = connection
         self.metadata = sa.MetaData(connection)
         self.metadata.reflect(bind=self.connection.engine)
+        self.passthrough_args = passthrough_args
         self.table_names = self._get_table_names()
         self.es = ft.EntitySet(id=id)
 
     def _get_table_names(self):
         return list(self.metadata.tables.keys())
+
+    def _get_args_for_table(self, table_name):
+        return {k: v.get(table_name) for k, v in self.passthrough_args.items()}
 
     def _load_table(self, table_name):
         table = sa.Table(table_name, self.metadata, autoload=True)
@@ -40,11 +50,13 @@ class EntitySetLoaderFromSQL:
         if make_index:
             index_name = 'featuretools_sql_import_id'
         df = pd.read_sql(table.select(), table.metadata.bind)
+        table_args = self._get_args_for_table(table_name)
         return self.es.entity_from_dataframe(
             entity_id=table_name,
             dataframe=df,
             index=index_name,
-            make_index=make_index
+            make_index=make_index,
+            **table_args
         )
 
     def _column_fk_list(self, table, column_name):
@@ -69,7 +81,11 @@ class EntitySetLoaderFromSQL:
                     parent_table = fk.column.table.name
                     self._add_relationship(parent_table, parent_column, child_table, child_column)
 
-    def load_all_tables(self):
+    def load_tables(self, included_tables):
+        if included_tables is not None:
+            tables_to_load = set(self.table_names) & set(included_tables)
+            self.table_names = list(tables_to_load)
+
         for table_name in self.table_names:
             self.es = self._load_table(table_name)
 
--- featuretools/tests/io_tests/test_from_sql.py
+++ featuretools/tests/io_tests/test_from_sql.py
@@ -1,4 +1,6 @@
 # flake8: noqa: F811
+from unittest.mock import patch
+
 import pytest
 
 import featuretools as ft
@@ -37,3 +39,18 @@ def test_infers_relationships(sqlite):
 def test_throws_error_on_composite_primary_key(sqlite_composite_pk):
     with pytest.raises(RuntimeError, match="Composite primary key detected"):
         ft.io.from_sql(id="composite_pk", connection=sqlite_composite_pk)
+
+
+def test_passes_through_parameters(sqlite):
+    es = ft.io.from_sql(id="widget_es", connection=sqlite, variable_types={"widgets": {'name': ft.variable_types.Categorical}})
+    assert(es.entity_dict['widgets']['name'].dtype == 'categorical')
+
+
+def test_throws_an_error_without_connection_or_connection_string(sqlite):
+    with pytest.raises(ValueError, match="connection or connection_string is required"):
+        ft.io.from_sql(id="no_connection")
+
+def test_loads_only_specified_tables(sqlite):
+    es = ft.io.from_sql(id="widget_limited_tables", connection=sqlite, tables=['widgets', 'factories'])
+    assert(len(es.entities) == 2)
+    assert(len(es.relationships) == 1)
