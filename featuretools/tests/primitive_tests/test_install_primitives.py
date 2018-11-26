fe72e9f238cdac6b004b981b7a291d055cb88271
Max Kanter <kmax12@gmail.com>
2018-11-25 23:07:08 -0500

    separate tests

--- featuretools/tests/primitive_tests/test_install_primitives.py
+++ featuretools/tests/primitive_tests/test_install_primitives.py
@@ -31,7 +31,7 @@ def primitives_to_install_archive():
     return os.path.join(this_dir, "primitives_to_install.tar.gz")
 
 
-def test_install_primitives(primitives_to_install_dir, primitives_to_install_archive):
+def setup_installation():
     installation_dir = get_installation_dir()
     custom_max_file = os.path.join(installation_dir, "custom_max.py")
     custom_mean_file = os.path.join(installation_dir, "custom_mean.py")
@@ -44,27 +44,59 @@ def test_install_primitives(primitives_to_install_dir, primitives_to_install_arc
         except Exception:
             pass
 
-    # test install from directory and archive
+
+def cleanup_installation():
+    installation_dir = get_installation_dir()
+    files = list_primitive_files(installation_dir)
+    # then delete to clean up
+    for f in files:
+        os.unlink(f)
+
+
+def do_installation(install_path):
+    install_primitives(install_path, prompt=False)
+
+    # due to how python modules are loaded/reloaded check for installed
+    # primitives in subprocesses
+    result = str(subprocess.check_output(['featuretools', "list-primitives"]))
+
+    # make sure the custom primitives are there
+    assert "custommax" in result
+    assert "custommean" in result
+    assert "customsum" in result
+
+    installation_dir = get_installation_dir()
+    files = list_primitive_files(installation_dir)
+    custom_max_file = os.path.join(installation_dir, "custom_max.py")
+    custom_mean_file = os.path.join(installation_dir, "custom_mean.py")
+    custom_sum_file = os.path.join(installation_dir, "custom_sum.py")
+    assert set(files) == {custom_max_file, custom_mean_file, custom_sum_file}
+
+
+def test_install_primitives_directory(primitives_to_install_dir):
+    setup_installation()
+    do_installation(primitives_to_install_dir)
+    cleanup_installation()
+
+
+def test_install_primitives_archive(primitives_to_install_archive):
+    setup_installation()
+    do_installation(primitives_to_install_archive)
+    cleanup_installation()
+
+
+def test_install_primitives_s3(primitives_to_install_dir, primitives_to_install_archive):
+    setup_installation()
     s3_archive = "s3://featuretools-static/primitives_to_install.tar.gz"
+    do_installation(s3_archive)
+    cleanup_installation()
+
+
+def test_install_primitives_https(primitives_to_install_dir, primitives_to_install_archive):
+    setup_installation()
     https_archive = "https://s3.amazonaws.com/featuretools-static/primitives_to_install.tar.gz"
-    for install_path in [primitives_to_install_dir, primitives_to_install_archive, s3_archive, https_archive]:
-        install_primitives(install_path, prompt=False)
-
-        # due to how python modules are loaded/reloaded check for installed
-        # primitives in subprocesses
-        result = str(subprocess.check_output(['featuretools', "list-primitives"]))
-
-        # make sure the custom primitives are there
-        assert "custommax" in result
-        assert "custommean" in result
-        assert "customsum" in result
-
-        files = list_primitive_files(installation_dir)
-        assert set(files) == {custom_max_file, custom_mean_file, custom_sum_file}
-
-        # then delete to clean up
-        for f in files:
-            os.unlink(f)
+    do_installation(https_archive)
+    cleanup_installation()
 
 
 def test_list_primitive_files(primitives_to_install_dir):
