917f9a3c297834023cea338a7f62c3d067285a8c
Bukosabino <bukosabino@gmail.com>
2019-01-13 18:08:08 +0100

    Generalize infer_variable_types, convert_variable_data and convert_all_variable_data methods https://github.com/Featuretools/featuretools/issues/338

--- featuretools/utils/api.py
+++ featuretools/utils/api.py
@@ -1,4 +1,10 @@
 # flake8: noqa
+from .entity_utils import (
+    _col_is_datetime,
+    convert_all_variable_data,
+    convert_variable_data,
+    infer_variable_types
+)
 from .gen_utils import is_string, make_tqdm_iterator
 from .pickle_utils import load_features, save_features
 from .time_utils import make_temporal_cutoffs
