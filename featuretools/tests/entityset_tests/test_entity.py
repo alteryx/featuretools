0a92950dd65810e8c393cc3b29d0913f937503ff
Gaurav Sheni <gvsheni@gmail.com>
2019-02-12 15:58:46 -0500

    added ips and email address to mock ds (#426)
    * added ips and email address to mock ds
    
    * change to address
    
    * lint
    
    * move mock ds
    
    * fix match text
    
    * fix test

--- featuretools/tests/entityset_tests/test_entity.py
+++ featuretools/tests/entityset_tests/test_entity.py
@@ -75,10 +75,10 @@ def test_update_data(es):
         es['customers'].update_data(df.drop(columns=['cohort']))
     assert 'Updated dataframe is missing new cohort column' in str(excinfo)
 
-    error_text = 'Updated dataframe contains 13 columns, expecting 12'
+    error_text = 'Updated dataframe contains 14 columns, expecting 13'
     with pytest.raises(ValueError, match=error_text) as excinfo:
         es['customers'].update_data(df)
-    assert 'Updated dataframe contains 13 columns, expecting 12' in str(excinfo)
+    assert 'Updated dataframe contains 14 columns, expecting 13' in str(excinfo)
 
     # test already_sorted on entity without time index
     df = es["sessions"].df.copy(deep=True)
