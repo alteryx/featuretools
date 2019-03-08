6ce66a2b9a3b7c0dc315a9174d1fb8dc3053f17c
Charles Bradshaw <bradshawce@vcu.edu>
2019-03-08 17:08:26 -0500

    Added try except to entry points

--- featuretools/__init__.py
+++ featuretools/__init__.py
@@ -19,6 +19,9 @@ __version__ = '0.6.1'
 
 import pkg_resources
 for entry_point in pkg_resources.iter_entry_points('featuretools_initialize'):
-    module = entry_point.load()
-    if hasattr(module, 'initialize'):
-        module.initialize()
+    try:
+        module = entry_point.load()
+        if hasattr(module, 'initialize'):
+            module.initialize()
+    except:
+        pass
--- featuretools/primitives/__init__.py
+++ featuretools/primitives/__init__.py
@@ -3,7 +3,10 @@ from .api import *
 
 import pkg_resources
 for entry_point in pkg_resources.iter_entry_points('featuretools_primitives'):
-    loaded = entry_point.load()
-    if hasattr(loaded, 'primitives'):
-        for name, obj in loaded.primitives.items():
-            globals()[name] = obj
+    try:
+        loaded = entry_point.load()
+        if hasattr(loaded, 'primitives'):
+            for name, obj in loaded.primitives.items():
+                globals()[name] = obj
+    except:
+        pass
