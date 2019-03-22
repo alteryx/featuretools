03f1a41198f5c0e14fa187ce053cfa46353e3d9e
Roy Wedge <roy.wedge@gmail.com>
2019-03-22 15:46:30 -0400

    add comments about time where we exclude the groupby feature when using all base features

--- featuretools/computational_backends/pandas_backend.py
+++ featuretools/computational_backends/pandas_backend.py
@@ -350,6 +350,7 @@ class PandasBackend(ComputationalBackend):
         for index, group in frame.groupby(groupby):
             for f in features:
                 column_names = [bf.get_name() for bf in f.base_features]
+                # exclude the groupby variable from being passed to the function
                 variable_data = [group[name] for name in column_names[:-1]]
                 feature_func = f.get_function()
 
