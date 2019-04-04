ca4ad1e06dc06094e16a9b741db09850707f16d0
Jeff Hernandez <inboxjeff@outlook.com>
2019-04-04 16:01:17 -0500

    upload tests

--- featuretools/tests/primitive_tests/test_primitive_base.py
+++ featuretools/tests/primitive_tests/test_primitive_base.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import pandas as pd
+from pytest import raises
 
 from featuretools.primitives import IsNull, Max
 from featuretools.primitives.base import PrimitiveBase, make_agg_primitive
@@ -26,11 +27,8 @@ def test_uses_calc_time():
         time_since = time - values.iloc[0]
         return time_since.total_seconds()
 
-    TimeSinceLast = make_agg_primitive(time_since_last,
-                                       [DatetimeTimeIndex],
-                                       Numeric,
-                                       name="time_since_last",
-                                       uses_calc_time=True)
+    TimeSinceLast = make_agg_primitive(
+        time_since_last, [DatetimeTimeIndex], Numeric, name="time_since_last", uses_calc_time=True)
     primitive = TimeSinceLast()
     datetimes = pd.Series([datetime(2015, 6, 7), datetime(2015, 6, 6)])
     answer = 86400.0
@@ -42,7 +40,9 @@ def test_call_multiple_args():
         def get_function(self):
             def test(x, y):
                 return y
+
             return test
+
     primitive = TestPrimitive()
     assert pd.Series([0, 1]).equals(primitive(range(1), range(2)))
     assert pd.Series([0, 1]).equals(primitive(range(1), range(2)))
@@ -58,9 +58,50 @@ def test_get_function_called_once():
 
             def test(x):
                 return x
+
             return test
 
     primitive = TestPrimitive()
     primitive(range(6))
     primitive(range(6))
     assert primitive.get_function_call_count == 1
+
+
+def test_args_string():
+    class Primitive(PrimitiveBase):
+        def __init__(self, bool=True, int=0, float=None):
+            self.bool = bool
+            self.int = int
+            self.float = float
+
+    primitive = Primitive(bool=True, int=4, float=.1)
+    string = primitive.get_args_string()
+    assert string == ', int=4, float=0.1'
+
+
+def test_args_string_default():
+    class Primitive(PrimitiveBase):
+        def __init__(self, bool=True, int=0, float=None):
+            self.bool = bool
+            self.int = int
+            self.float = float
+
+    string = Primitive().get_args_string()
+    assert string == ''
+
+
+def test_args_string_undefined():
+    class Primitive(PrimitiveBase):
+        pass
+
+    string = Primitive().get_args_string()
+    assert string == ''
+
+
+def test_args_string_error():
+    class Primitive(PrimitiveBase):
+        def __init__(self, bool=True, int=0, float=None):
+            pass
+
+    with raises(ValueError, match='must be attribute'):
+        Primitive(bool=True, int=4, float=.1).get_args_string()
