db01c14b7c0f234958685fad6219c28bd3722b87
thehomebrewnerd <4307001+thehomebrewnerd@users.noreply.github.com>
2019-04-12 12:42:51 -0500

    fix failing tests

--- featuretools/tests/utils_tests/test_list_primitives.py
+++ featuretools/tests/utils_tests/test_list_primitives.py
@@ -28,8 +28,8 @@ def test_list_primitives_order():
 
 
 def test_descriptions():
-    primitives = {NumCharacters: 'Return the number of characters in a given string.',
-                  Day: 'Transform a Datetime feature into the day.',
+    primitives = {NumCharacters: 'Calculates the number of characters in a string.',
+                  Day: 'Determines the day value of a datetime.',
                   Last: 'Returns the last value.',
                   GreaterThan: ''}
     assert _get_descriptions(list(primitives.keys())) == list(primitives.values())
