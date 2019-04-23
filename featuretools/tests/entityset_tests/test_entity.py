04187b50075fc8f0ff384601f1645e7b629037d4
Geoff Kaufman <geoff.kaufman2@gmail.com>
2019-04-23 15:21:15 -0700

    update test_entity.py

--- featuretools/tests/entityset_tests/test_entity.py
+++ featuretools/tests/entityset_tests/test_entity.py
@@ -76,10 +76,10 @@ def test_update_data(es):
         es['customers'].update_data(df.drop(columns=['cohort']))
     assert 'Updated dataframe is missing new cohort column' in str(excinfo)
 
-    error_text = 'Updated dataframe contains 15 columns, expecting 14'
+    error_text = 'Updated dataframe contains 16 columns, expecting 15'
     with pytest.raises(ValueError, match=error_text) as excinfo:
         es['customers'].update_data(df)
-    assert 'Updated dataframe contains 15 columns, expecting 14' in str(excinfo)
+    assert 'Updated dataframe contains 16 columns, expecting 15' in str(excinfo)
 
     # test already_sorted on entity without time index
     df = es["sessions"].df.copy(deep=True)
