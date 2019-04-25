b35d0c46ed82793722c7dfdab59dfc0c75b1c119
Roy Wedge <roy.wedge@gmail.com>
2019-04-25 11:10:36 -0400

    Remove build_ext function from setup

--- setup.py
+++ setup.py
@@ -1,23 +1,10 @@
 from os import path
 
 from setuptools import find_packages, setup
-from setuptools.command.build_ext import build_ext as _build_ext
 
 dirname = path.abspath(path.dirname(__file__))
 with open(path.join(dirname, 'README.md')) as f:
     long_description = f.read()
-
-
-# Bootstrap numpy install
-class build_ext(_build_ext):
-
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
-        import numpy
-        self.include_dirs.append(numpy.get_include())
-
 
 setup(
     name='featuretools',
