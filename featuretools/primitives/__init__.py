2f4e41651df29893c71e50b0ba1f2577e933a5c7
Roy Wedge <rwedge@featurelabs.com>
2019-05-03 11:27:54 -0400

    add example use of entry point

--- featuretools/primitives/__init__.py
+++ featuretools/primitives/__init__.py
@@ -2,7 +2,13 @@
 from .api import *
 
 import pkg_resources
-# Load in primitives registered by other libraries into Featuretools namespace
+# Load in a list of primitives registered by other libraries into Featuretools
+# Example entry_points definition for a library using this entry point:
+#    entry_points={
+#        "featuretools_primitives": [
+#            other_library = other_library:LIST_OF_PRIMITIVES
+#        ]
+#    }
 for entry_point in pkg_resources.iter_entry_points('featuretools_primitives'):
     try:
         loaded = entry_point.load()
