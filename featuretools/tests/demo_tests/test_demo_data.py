1cdad1be41a7fa99988c36bacc14c43fb8e856c7
Gaurav Sheni <gvsheni@gmail.com>
2019-05-16 16:54:21 -0400

    fix fixture

--- featuretools/tests/demo_tests/test_demo_data.py
+++ featuretools/tests/demo_tests/test_demo_data.py
@@ -12,12 +12,13 @@ else:
 
 @pytest.fixture(autouse=True)
 def set_testing_headers():
+    print('set_testing_headers')
     opener = urllib2.build_opener()
     opener.addheaders = [('Testing', 'True')]
     urllib2.install_opener(opener)
 
 
-def test_load_retail_diff(set_testing_headers):
+def test_load_retail_diff():
     nrows = 10
     es_first = load_retail(nrows=nrows)
     assert es_first['order_products'].df.shape[0] == nrows
@@ -26,14 +27,14 @@ def test_load_retail_diff(set_testing_headers):
     assert es_second['order_products'].df.shape[0] == nrows_second
 
 
-def test_mock_customer(set_testing_headers):
+def test_mock_customer():
     es = load_mock_customer(return_entityset=True)
     fm, fl = dfs(entityset=es, target_entity="customers", max_depth=3)
     for feature in fl:
         assert feature.get_name() in fm.columns
 
 
-def test_load_flight(set_testing_headers):
+def test_load_flight():
     es = load_flight(month_filter=[1],
                      categorical_filter={'origin_city': ['Charlotte, NC']},
                      return_single_table=False, nrows=1000)
