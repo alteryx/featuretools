2369473dba0c4e9c89118809a30c7fd9212be191
Gaurav Sheni <gvsheni@gmail.com>
2019-05-16 15:08:34 -0400

    use testing heaader

--- featuretools/tests/demo_tests/test_demo_data.py
+++ featuretools/tests/demo_tests/test_demo_data.py
@@ -1,29 +1,36 @@
 from featuretools.demo import load_flight, load_mock_customer, load_retail
 from featuretools.synthesis import dfs
+import urllib.request
+import pytest
 
 
-def test_load_retail_diff():
+@pytest.fixture
+def fix_headers():
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('Testing', 'True')]
+    urllib.request.install_opener(opener)
+
+
+def test_load_retail_diff(fix_headers):
     nrows = 10
     es_first = load_retail(nrows=nrows)
     assert es_first['order_products'].df.shape[0] == nrows
-
     nrows_second = 11
     es_second = load_retail(nrows=nrows_second)
     assert es_second['order_products'].df.shape[0] == nrows_second
 
 
-def test_mock_customer():
+def test_mock_customer(fix_headers):
     es = load_mock_customer(return_entityset=True)
     fm, fl = dfs(entityset=es, target_entity="customers", max_depth=3)
     for feature in fl:
         assert feature.get_name() in fm.columns
 
 
-def test_load_flight():
+def test_load_flight(fix_headers):
     es = load_flight(month_filter=[1],
                      categorical_filter={'origin_city': ['Charlotte, NC']},
                      return_single_table=False, nrows=1000)
-
     entity_names = ['airports', 'flights', 'trip_logs', 'airlines']
     realvals = [(11, 3), (13, 9), (103, 21), (1, 1)]
     for i, name in enumerate(entity_names):
