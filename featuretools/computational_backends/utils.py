db96c399628dfcdc6db63bea92286bccbb88320f
Gaurav <gvsheni@gmail.com>
2019-07-01 18:14:24 -0400

    moved utils import

--- featuretools/computational_backends/utils.py
+++ featuretools/computational_backends/utils.py
@@ -9,7 +9,6 @@ import numpy as np
 import pandas as pd
 import psutil
 from pandas.tseries.frequencies import to_offset
-from distributed import Client, LocalCluster
 
 from featuretools.entityset.relationship import RelationshipPath
 from featuretools.feature_base import AggregationFeature, DirectFeature
@@ -210,6 +209,8 @@ def n_jobs_to_workers(n_jobs):
 
 
 def create_client_and_cluster(n_jobs, num_tasks, dask_kwargs, entityset_size):
+    from distributed import Client, LocalCluster
+
     cluster = None
     if 'cluster' in dask_kwargs:
         cluster = dask_kwargs['cluster']
