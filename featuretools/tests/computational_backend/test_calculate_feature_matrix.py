b3892ae64cad2a48f8e50c114a66e6d1f041dc29
Gaurav <gvsheni@gmail.com>
2019-07-02 10:47:17 -0400

    fix lint

--- featuretools/tests/computational_backend/test_calculate_feature_matrix.py
+++ featuretools/tests/computational_backend/test_calculate_feature_matrix.py
@@ -22,7 +22,6 @@ from featuretools.computational_backends.utils import (
     bin_cutoff_times,
     calc_num_per_chunk,
     create_client_and_cluster,
-    get_client_cluster,
     get_next_chunk,
     n_jobs_to_workers
 )
