767732e00f9a47f5edfa340a557c404733234f69
Max Kanter <kmax12@gmail.com>
2019-07-23 17:31:41 -0400

    remove bare except

--- featuretools/utils/entity_utils.py
+++ featuretools/utils/entity_utils.py
@@ -180,7 +180,7 @@ def col_is_datetime(col):
     # if it can be casted to numeric, it's not a datetime
     try:
         pd.to_numeric(col.dropna().iloc[:10], errors='raise')
-    except:
+    except ValueError:
         # finally, try to cast to datetime
         if col.dtype.name.find('str') > -1 or col.dtype.name.find('object') > -1:
             try:
