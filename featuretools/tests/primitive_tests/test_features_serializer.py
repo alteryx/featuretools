425e6fcdd217963ee924f53f73b8455f6809270b
Jeremy Shih <jeremyliweishih@gmail.com>
2019-08-06 11:48:11 -0400

    Fixed schema version in tests

--- featuretools/tests/primitive_tests/test_features_serializer.py
+++ featuretools/tests/primitive_tests/test_features_serializer.py
@@ -2,7 +2,7 @@ import featuretools as ft
 from featuretools.entityset.deserialize import description_to_entityset
 from featuretools.feature_base.features_serializer import FeaturesSerializer
 
-SCHEMA_VERSION = "3.0.0"
+SCHEMA_VERSION = "3.0.1"
 
 
 def test_single_feature(es):
