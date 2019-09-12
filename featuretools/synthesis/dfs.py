381104385bdd60f04abda0bc293580418f2e8a67
frances-h <frances.hartwell@featurelabs.com>
2019-09-12 16:16:06 -0400

    update dfs docstring

--- featuretools/synthesis/dfs.py
+++ featuretools/synthesis/dfs.py
@@ -20,6 +20,7 @@ def dfs(entities=None,
         max_depth=2,
         ignore_entities=None,
         ignore_variables=None,
+        primitive_options=None,
         seed_features=None,
         drop_contains=None,
         drop_exact=None,
@@ -89,6 +90,39 @@ def dfs(entities=None,
 
         ignore_variables (dict[str -> list[str]], optional): List of specific
             variables within each entity to blacklist when creating features.
+
+        primitive_options (dict[str or list[str] -> dict, optional]): Specify
+            options or a single primitive or a group of primitives. Each option
+            ``dict`` can have the following keys:
+
+            ``"include_entities"``
+                List of entities to be included when creating features for
+                the primitive(s). All other entities will be ignored
+                (list[str]).
+            ``"ignore_entities"``
+                List of entities to be blacklisted when creating features
+                for the primitive(s) (list[str]).
+            ``"include_variables"``
+                List of specific variables within each entity to include when
+                creating feautres for the primitive(s). All other variables
+                in a given entity will be ignored (dict[str -> list[str]]).
+            ``"ignore_variables"``
+                List of specific variables within each entityt to blacklist
+                when creating features for the primitive(s) (dict[str ->
+                list[str]]).
+            ``"include_groupby_entities"``
+                List of Entities to be included when finding groupbys. All
+                other entities will be ignored (list[str]).
+            ``"ignore_groupby_entities"``
+                List of entities to blacklist when finding groupbys
+                (list[str]).
+            ``"include_groupby_variables"``
+                List of specific variables within each entity to include as
+                groupbys, if applicable. All other variables in each
+                entity will be ignored (dict[str -> list[str]]).
+            ``"ignore_groupby_variables"``
+                List of specific variables within each entity to blacklist
+                as groupbys (dict[str -> list[str]]).
 
         seed_features (list[:class:`.FeatureBase`]): List of manually defined
             features to use.
