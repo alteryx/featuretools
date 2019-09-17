837543864afcfa6c48b020d15b33ae2b4cb16f5b
christopherbunn <chris.l.bunn@gmail.com>
2019-09-17 10:04:37 -0400

    Fixed SCHEMA_VERSION in test_feature_serialization

--- featuretools/tests/primitive_tests/test_features_serializer.py
+++ featuretools/tests/primitive_tests/test_features_serializer.py
@@ -4,7 +4,7 @@ import featuretools as ft
 from featuretools.entityset.deserialize import description_to_entityset
 from featuretools.feature_base.features_serializer import FeaturesSerializer
 
-SCHEMA_VERSION = "3.1.0"
+SCHEMA_VERSION = "3.2.0"
 
 
 def test_single_feature(es):
