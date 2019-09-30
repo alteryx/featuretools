0f232dd0846213541f83d137ae7c569956cee42e
Roy Wedge <rwedge@featurelabs.com>
2019-09-30 12:47:41 -0400

    bump rc number

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ extras_require['complete'] = sorted(set(sum(extras_require.values(), [])))
 
 setup(
     name='featuretools',
-    version='0.11.0rc1',
+    version='0.11.0rc2',
     packages=find_packages(),
     description='a framework for automated feature engineering',
     url='http://featuretools.com',
