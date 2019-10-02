287d916f87f36ab175b154c7766ebdf9f7d8c3e3
Roy Wedge <rwedge@featurelabs.com>
2019-10-02 10:40:43 -0400

    remove utf-8 coding headers since it is default in python3

--- featuretools/tests/entityset_tests/test_es.py
+++ featuretools/tests/entityset_tests/test_es.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import copy
 from builtins import range
 from datetime import datetime
--- featuretools/tests/testing_utils/mock_ds.py
+++ featuretools/tests/testing_utils/mock_ds.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import os
 from datetime import datetime
 
