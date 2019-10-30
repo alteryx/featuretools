9aae867d34ad4b7ae49681578ea1998cf1b74f7f
Roy Wedge <rwedge@featurelabs.com>
2019-10-30 18:41:32 -0400

    bump version number

--- featuretools/tests/test_version.py
+++ featuretools/tests/test_version.py
@@ -2,4 +2,4 @@ from featuretools import __version__
 
 
 def test_version():
-    assert __version__ == "0.12.0.dev0"
+    assert __version__ == "0.12.0.dev1"
--- featuretools/version.py
+++ featuretools/version.py
@@ -1,1 +1,1 @@
-__version__ = '0.12.0.dev0'
+__version__ = '0.12.0.dev1'
