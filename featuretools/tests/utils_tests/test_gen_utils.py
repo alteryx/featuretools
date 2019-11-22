65c34e9541af23bc7c6a06c32bc06f71af48cef7
Roy Wedge <rwedge@featurelabs.com>
2019-11-22 08:58:54 -0500

    add test for import_or_raise

---
+++ featuretools/tests/utils_tests/test_gen_utils.py
@@ -0,0 +1,8 @@
+import pytest
+
+from featuretools.utils.gen_utils import import_or_raise
+
+
+def test_import_or_raise_errors():
+    with pytest.raises(ImportError, match="error message"):
+        _featuretools = import_or_raise("_featuretools", "error message")
