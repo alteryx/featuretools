fb6b89ac994740cb5e0cac424dd026539ee8a27f
Roy Wedge <rwedge@featurelabs.com>
2019-12-11 11:44:23 -0500

    remove wrappers

--- featuretools/wrappers/sklearn.py
+++
@@ -1,8 +0,0 @@
-from featuretools.utils.gen_utils import import_or_raise
-
-SKLEARN_ERR_MSG = (
-    "The featuretools_sklearn_transformer library is required to use DFSTransformer.\n"
-    "Install via pip:\n"
-    "    pip install featuretools_sklearn_transformer\n"
-)
-DFSTransformer = import_or_raise("featuretools_sklearn_transformer", SKLEARN_ERR_MSG).DFSTransformer
