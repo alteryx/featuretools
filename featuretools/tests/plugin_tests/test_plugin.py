0ef44d71b401679fba85d94966b831d1f9097fb3
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2020-02-24 17:04:26 -0600

    pip

---
+++ featuretools/tests/plugin_tests/test_plugin.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+import sys
+
+PWD = os.path.dirname(__file__)
+
+
+def pip(*args):
+    command = [sys.executable, '-m', 'pip', *args]
+    code = subprocess.run(command).returncode
+    return code
