5a8fa8645b1dfed9d2e61bf6d38004a812defcfc
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2020-03-02 11:51:21 -0600

    removed pandas requirement

--- featuretools/tests/plugin_tests/featuretools_plugin/setup.py
+++ featuretools/tests/plugin_tests/featuretools_plugin/setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(
     name='featuretools_plugin',
     packages=['featuretools_plugin'],
-    install_requires=['pandas<0.24.0,>=0.23.0'],
     entry_points={
         'featuretools_plugin': [
             'module = featuretools_plugin',
