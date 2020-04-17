aecbf1a0bb25ec561f03f4a56b440d70bf74f845
Roy Wedge <rwedge@featurelabs.com>
2020-04-17 17:46:51 -0400

    remove py35 from setup files and docs

--- setup.py
+++ setup.py
@@ -29,12 +29,11 @@ setup(
          'Development Status :: 3 - Alpha',
          'Intended Audience :: Developers',
          'Programming Language :: Python :: 3',
-         'Programming Language :: Python :: 3.5',
          'Programming Language :: Python :: 3.6',
          'Programming Language :: Python :: 3.7'
     ],
     install_requires=open('requirements.txt').readlines(),
-    python_requires='>=3.5, <4',
+    python_requires='>=3.6, <4',
     extras_require=extras_require,
     keywords='feature engineering data science machine learning',
     include_package_data=True,
