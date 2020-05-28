750c5fabdaa621374b0c9a9c8c82457b4822f8fc
tuethan1999 <ethan.tu@alteryx.com>
2020-05-28 10:25:37 -0400

    fixed typo

--- featuretools/variable_types/variable.py
+++ featuretools/variable_types/variable.py
@@ -5,7 +5,7 @@ from featuretools.utils.gen_utils import camel_to_snake, find_descendents
 
 
 class ClassNameDescriptor(object):
-    """Descriptor to convert a class's name from camelcase to snakcase
+    """Descriptor to convert a class's name from camelcase to snakecase
     """
     def __get__(self, instance, class_):
         return camel_to_snake(class_.__name__)
