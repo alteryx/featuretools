ed409577bcb55f92a5390ca239d5620fea1637eb
Gaurav Sheni <gaurav.sheni@alteryx.com>
2020-06-10 19:38:15 -0400

    lint

--- featuretools/tests/entityset_tests/test_serialization.py
+++ featuretools/tests/entityset_tests/test_serialization.py
@@ -9,10 +9,10 @@ from dask import dataframe as dd
 from featuretools.demo import load_mock_customer
 from featuretools.entityset import EntitySet, deserialize, serialize
 from featuretools.variable_types import (
-    find_variable_types,
     Categorical,
     Index,
-    TimeIndex
+    TimeIndex,
+    find_variable_types
 )
 
 BUCKET_NAME = "test-bucket"
--- featuretools/variable_types/api.py
+++ featuretools/variable_types/api.py
@@ -1,7 +1,7 @@
 # flake8: noqa
-from .variable import *
 from .utils import (
     find_variable_types,
-    list_variable_types,
-    graph_variable_types
+    graph_variable_types,
+    list_variable_types
 )
+from .variable import *
