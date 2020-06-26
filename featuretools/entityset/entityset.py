95117a7da15decf993bde65479f67fe883072754
Frances Hartwell <frances.hartwell@alteryx.com>
2020-06-26 15:31:36 -0400

    Error if relationship child variable is child entity index (#1034)
    * error if child variable is child index
    
    * changelog
    
    * update error message

--- featuretools/entityset/entityset.py
+++ featuretools/entityset/entityset.py
@@ -248,6 +248,9 @@ class EntitySet(object):
         # this is a new pair of entities
         child_e = relationship.child_entity
         child_v = relationship.child_variable.id
+        if child_e.index == child_v:
+            msg = "Unable to add relationship because child variable '{}' in '{}' is also its index"
+            raise ValueError(msg.format(child_v, child_e.id))
         parent_e = relationship.parent_entity
         parent_v = relationship.parent_variable.id
         if not isinstance(child_e[child_v], vtypes.Id):
