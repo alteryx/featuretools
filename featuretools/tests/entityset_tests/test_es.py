394698ee5674efdf908142bc661966a2bc09691e
Nate Parsons <4307001+thehomebrewnerd@users.noreply.github.com>
2020-08-18 16:40:34 -0500

    Fix Koalas entity creation performance issue (#1114)
    * fix entity creation performance issue
    
    * fix test

--- featuretools/tests/entityset_tests/test_es.py
+++ featuretools/tests/entityset_tests/test_es.py
@@ -897,8 +897,8 @@ def test_sets_time_when_adding_entity(transactions_df):
 
 
 def test_checks_time_type_setting_time_index(es):
-    # set non time type as time index, Dask and Pandas error differently
-    if isinstance(es['log'].df, (pd.DataFrame, ks.DataFrame)):
+    # set non time type as time index, Dask/Koalas and Pandas error differently
+    if isinstance(es['log'].df, pd.DataFrame):
         error_text = 'log time index not recognized as numeric or datetime'
     else:
         error_text = "log time index is %s type which differs from" \
