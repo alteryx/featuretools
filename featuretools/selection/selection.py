0e41d9530b2eb2524e39117cb1f076a0c0c9de50
Tamar Grey <tamar.grey@alteryx.com>
2020-08-28 15:52:33 -0400

    Return actual changed matrix

--- featuretools/selection/selection.py
+++ featuretools/selection/selection.py
@@ -25,64 +25,70 @@ The three functions below use logic from EvalML DataChecks
 '''
 
 
-def find_highly_null_features(feature_matrix, pct_null_threshold=0.95):
+def remove_highly_null_features(feature_matrix, features=None, pct_null_threshold=0.95):
     """
-    Determine features from a feature matrix that have higher than a set threshold
-    of null values.
+        Determine features from a feature matrix that have higher than a set threshold
+        of null values.
 
-    Args:
-        feature_matrix (:class:`pd.DataFrame`): DataFrame whose columns are feature names and rows are instances
-        pct_null_threshold (float): If the percentage of NaN values in an input feature exceeds this amount,
-                that feature will be considered highly-null. Defaults to 0.95.
+        Args:
+            feature_matrix (:class:`pd.DataFrame`): DataFrame whose columns are feature names and rows are instances
+            features (list[:class:`featuretools.FeatureBase`] or list[str], optional): List of features to select
+            pct_null_threshold (float): If the percentage of NaN values in an input feature exceeds this amount,
+                    that feature will be considered highly-null. Defaults to 0.95.
 
-    Returns:
-        List of featxure names that will match columns in the inputted feature matrix
-        where the null percentage was above the set threshold
-
+        Returns:
+            feature matrix (pd.DataFrame): The feature matrix generated. If `features_only` is ``True``,
+                the feature matrix will not be generated.
+            features (list[:class:`.FeatureBase`]): The list of generated feature defintions.
     """
     if pct_null_threshold < 0 or pct_null_threshold > 1:
         raise ValueError("pct_null_threshold must be a float between 0 and 1, inclusive.")
 
     percent_null_by_col = (feature_matrix.isnull().mean()).to_dict()
-    # --> Should we be returning the actual features here? something else?
-    if pct_null_threshold == 0.0:
-        return [f_name for f_name, pct_null in percent_null_by_col.items() if pct_null > 0.0]
+    keep = [f_name for f_name, pct_null in percent_null_by_col.items() if pct_null < pct_null_threshold]
 
-    return [f_name for f_name, pct_null in percent_null_by_col.items() if pct_null >= pct_null_threshold]
+    return _apply_feature_selection(keep, feature_matrix, features)
 
 
-def find_single_value_features(feature_matrix, count_nan_as_value=False):
+def remove_single_value_features(feature_matrix, features=None, count_nan_as_value=False):
     """
-    Determines columns in feature matrix where all the values are the same.
+        Determines columns in feature matrix where all the values are the same.
 
-    Args:
-        feature_matrix (:class:`pd.DataFrame`): DataFrame whose columns are feature names and rows are instances
-        count_nan_as_value (bool): If True, missing values will be counted as their own unique value.
-                    If set to True, a feature that has one unique value and all other data is missing will be
-                    counted as only having a single unique value. Defaults to False.
+        Args:
+            feature_matrix (:class:`pd.DataFrame`): DataFrame whose columns are feature names and rows are instances
+            features (list[:class:`featuretools.FeatureBase`] or list[str], optional): List of features to select
+            count_nan_as_value (bool): If True, missing values will be counted as their own unique value.
+                        If set to True, a feature that has one unique value and all other data is missing will be
+                        counted as only having a single unique value. Defaults to False.
 
-    Returns:
-        List of feature names of all the single value features - note None is not counted
-        as a value, so "single value" is either 0 or 1 unique values
+        Returns:
+            feature matrix (pd.DataFrame): The feature matrix generated. If `features_only` is ``True``,
+                the feature matrix will not be generated.
+            features (list[:class:`.FeatureBase`]): The list of generated feature defintions.
     """
     unique_counts_by_col = feature_matrix.nunique(dropna=not count_nan_as_value).to_dict()
-    return [f_name for f_name, unique_count in unique_counts_by_col.items() if unique_count <= 1]
+
+    keep = [f_name for f_name, unique_count in unique_counts_by_col.items() if unique_count > 1]
+    return _apply_feature_selection(keep, feature_matrix, features)
 
 
-def find_highly_correlated_features(feature_matrix, pct_corr_threshold=0.95, features_to_check=None, features_to_exclude=None):
+def remove_highly_correlated_features(feature_matrix, features=None, pct_corr_threshold=0.95, features_to_check=None, features_to_exclude=None):
     """
-    Determines whether any pairs of features are highly correlated with one another.
+        Determines whether any pairs of features are highly correlated with one another.
 
-    Args:
-        feature_matrix (:class:`pd.DataFrame`): DataFrame whose columns are feature names and rows are instances
-        pct_corr_threshold (float): The correlation threshold to be considered highly correlated. Defaults to 0.95.
-        cols_to_check (set{str}, optional): Set of column names to check whether any pairs are highly correlated.
-                    If null, defaults to checking all columns.
-        cols_to_exclude set[str], optional): Set of colum names to not check correlation between.
-                    If null, will not exclude any columns.
+        Args:
+            feature_matrix (:class:`pd.DataFrame`): DataFrame whose columns are feature names and rows are instances
+            features (list[:class:`featuretools.FeatureBase`] or list[str], optional): List of features to select
+            pct_corr_threshold (float): The correlation threshold to be considered highly correlated. Defaults to 0.95.
+            cols_to_check (set{str}, optional): Set of column names to check whether any pairs are highly correlated.
+                        If null, defaults to checking all columns.
+            cols_to_exclude set[str], optional): Set of colum names to not check correlation between.
+                        If null, will not exclude any columns.
 
-    Returns:
-        list[tuple(str, str)] of column pairs that is correlated above the set threshold.
+        Returns:
+            feature matrix (pd.DataFrame): The feature matrix generated. If `features_only` is ``True``,
+                the feature matrix will not be generated.
+            features (list[:class:`.FeatureBase`]): The list of generated feature defintions.
     """
     if pct_corr_threshold < 0 or pct_corr_threshold > 1:
         raise ValueError("pct_corr_threshold must be a float between 0 and 1, inclusive.")
@@ -103,17 +109,26 @@ def find_highly_correlated_features(feature_matrix, pct_corr_threshold=0.95, fea
 
     fm_to_check = (feature_matrix[features_to_check]).select_dtypes(include=numeric_and_boolean_dtypes)
 
-    correlation_by_pairs = {}
+    dropped = set()
     # Get all pairs of columns and calculate their correlation
     for f_name1, col1 in fm_to_check.iteritems():
         for f_name2, col2 in fm_to_check.iteritems():
-            pair = tuple(sorted((f_name1, f_name2)))
-            if f_name1 == f_name2 or pair in correlation_by_pairs:
+            if f_name1 == f_name2 or f_name1 in dropped or f_name2 in dropped:
                 continue
 
-            correlation_by_pairs[pair] = abs(col1.corr(col2))
+            if abs(col1.corr(col2)) >= pct_corr_threshold:
+                dropped.add(f_name1)
+                dropped.add(f_name2)
 
-    # --> might be better ways of presenting this info
-        # dict {col_name: [correlated columns]}
-        # just the list of columns that are highly correlated to another column?
-    return[f_pair for f_pair, correlation in correlation_by_pairs.items() if correlation >= pct_corr_threshold]
+    keep = [f_name for f_name in feature_matrix.columns if f_name not in dropped]
+    return _apply_feature_selection(keep, feature_matrix, features)
+
+
+def _apply_feature_selection(keep, feature_matrix, features=None):
+    new_matrix = feature_matrix[keep]
+    new_feature_names = set(new_matrix.columns)
+
+    if features is not None:
+        features = [f for f in features if f.get_name() in new_feature_names]
+        return new_matrix, features
+    return new_matrix
