02590cf5d94ce6840424cbfb38660494a361827b
Gaurav Sheni <gaurav.sheni@alteryx.com>
2020-08-31 17:09:18 -0400

    fix return type

--- featuretools/computational_backends/calculate_feature_matrix.py
+++ featuretools/computational_backends/calculate_feature_matrix.py
@@ -127,7 +127,7 @@ def calculate_feature_matrix(features, entityset=None, cutoff_time=None, instanc
         include_cutoff_time (bool): Include data at cutoff times in feature calculations. Defaults to ``True``.
 
     Returns:
-        feature matrix (pd.DataFrame): The feature matrix generated.
+        pd.DataFrame: The feature matrix generated.
     """
     assert (isinstance(features, list) and features != [] and
             all([isinstance(feature, FeatureBase) for feature in features])), \
