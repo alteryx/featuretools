136d85c4be7eff83c639beb44a0bfd79dd7c4d9b
Tamar Grey <tamar.grey@alteryx.com>
2020-09-04 18:35:56 -0400

    shorten lines

--- featuretools/selection/selection.py
+++ featuretools/selection/selection.py
@@ -45,9 +45,11 @@ def remove_highly_null_features(feature_matrix, features=None, pct_null_threshol
     percent_null_by_col = (feature_matrix.isnull().mean()).to_dict()
 
     if pct_null_threshold == 0.0:
-        keep = [f_name for f_name, pct_null in percent_null_by_col.items() if pct_null <= pct_null_threshold]
+        keep = [f_name for f_name, pct_null in percent_null_by_col.items()
+                if pct_null <= pct_null_threshold]
     else:
-        keep = [f_name for f_name, pct_null in percent_null_by_col.items() if pct_null < pct_null_threshold]
+        keep = [f_name for f_name, pct_null in percent_null_by_col.items()
+                if pct_null < pct_null_threshold]
 
     return _apply_feature_selection(keep, feature_matrix, features)
 
@@ -64,38 +66,48 @@ def remove_single_value_features(feature_matrix, features=None, count_nan_as_val
 
          Returns:
             pd.DataFrame, list[:class:`.FeatureBase`]:
-                The feature matrix and the list of generated feature definitions. Matches dfs output.
+                The feature matrix and the list of generated feature definitions.
+                Matches dfs output.
                 If no feature list is provided as input, the feature list will not be returned.
     """
     unique_counts_by_col = feature_matrix.nunique(dropna=not count_nan_as_value).to_dict()
 
-    keep = [f_name for f_name, unique_count in unique_counts_by_col.items() if unique_count > 1]
+    keep = [f_name for f_name, unique_count
+            in unique_counts_by_col.items() if unique_count > 1]
     return _apply_feature_selection(keep, feature_matrix, features)
 
 
-def remove_highly_correlated_features(feature_matrix, features=None, pct_corr_threshold=0.95, features_to_check=None, features_to_keep=None):
+def remove_highly_correlated_features(feature_matrix, features=None, pct_corr_threshold=0.95,
+                                      features_to_check=None, features_to_keep=None):
     """Removes columns in feature matrix that are highly correlated with another column.
 
         Note:
-            We make the assumption that, for a pair of features, the feature that is further right in the feature matrix
-            produced by ``dfs`` is the more complex one. The assumption does not hold if the order of columns
-            in the feature matrix has changed from what ``dfs`` produces.
+            We make the assumption that, for a pair of features, the feature that is further
+            right in the feature matrix produced by ``dfs`` is the more complex one.
+            The assumption does not hold if the order of columns in the feature
+            matrix has changed from what ``dfs`` produces.
 
         Args:
-            feature_matrix (:class:`pd.DataFrame`): DataFrame whose columns are feature names and rows are instances.
-            features (list[:class:`featuretools.FeatureBase`] or list[str], optional): List of features to select.
-            pct_corr_threshold (float): The correlation threshold to be considered highly correlated. Defaults to 0.95.
-            features_to_check (list[str], optional): List of column names to check whether any pairs are highly correlated.
-                        Will not check any other columns, meaning the only columns that can be removed are in this list.
-                        If null, defaults to checking all columns.
-            features_to_keep (list[str], optional): List of colum names to keep even if correlated to another column.
-                        If null, all columns will be candidates for removal.
+            feature_matrix (:class:`pd.DataFrame`): DataFrame whose columns are feature
+                        names and rows are instances.
+            features (list[:class:`featuretools.FeatureBase`] or list[str], optional):
+                        List of features to select.
+            pct_corr_threshold (float): The correlation threshold to be considered highly
+                        correlated. Defaults to 0.95.
+            features_to_check (list[str], optional): List of column names to check
+                        whether any pairs are highly correlated. Will not check any
+                        other columns, meaning the only columns that can be removed
+                        are in this list. If null, defaults to checking all columns.
+            features_to_keep (list[str], optional): List of colum names to keep even
+                        if correlated to another column. If null, all columns will be
+                        candidates for removal.
 
         Returns:
             pd.DataFrame, list[:class:`.FeatureBase`]:
-                The feature matrix and the list of generated feature definitions. Matches dfs output.
-                If no feature list is provided as input, the feature list will not be returned.
-                For consistent results, do not change the order of features outputted by dfs.
+                The feature matrix and the list of generated feature definitions.
+                Matches dfs output. If no feature list is provided as input,
+                the feature list will not be returned. For consistent results,
+                do not change the order of features outputted by dfs.
     """
     if pct_corr_threshold < 0 or pct_corr_threshold > 1:
         raise ValueError("pct_corr_threshold must be a float between 0 and 1, inclusive.")
@@ -112,11 +124,13 @@ def remove_highly_correlated_features(feature_matrix, features=None, pct_corr_th
     boolean = ['bool']
     numeric_and_boolean_dtypes = vtypes.PandasTypes._pandas_numerics + boolean
 
-    fm_to_check = (feature_matrix[features_to_check]).select_dtypes(include=numeric_and_boolean_dtypes)
+    fm_to_check = (feature_matrix[features_to_check]).select_dtypes(
+        include=numeric_and_boolean_dtypes)
 
     dropped = set()
     columns_to_check = fm_to_check.columns
-    # When two features are found to be highly correlated, we drop the more complex feature
+    # When two features are found to be highly correlated,
+    # we drop the more complex feature
     # Columns produced later in dfs are more complex
     for i in range(len(columns_to_check) - 1, 0, -1):
         more_complex_name = columns_to_check[i]
@@ -130,8 +144,8 @@ def remove_highly_correlated_features(feature_matrix, features=None, pct_corr_th
                 dropped.add(more_complex_name)
                 break
 
-    keep = [f_name for f_name in feature_matrix.columns if (f_name in features_to_keep or
-                                                            f_name not in dropped)]
+    keep = [f_name for f_name in feature_matrix.columns
+            if (f_name in features_to_keep or f_name not in dropped)]
     return _apply_feature_selection(keep, feature_matrix, features)
 
 
@@ -144,7 +158,8 @@ def _apply_feature_selection(keep, feature_matrix, features=None):
         for f in features:
             if f.number_output_features > 1:
 
-                slices = [f[i] for i in range(f.number_output_features) if f[i].get_name() in new_feature_names]
+                slices = [f[i] for i in range(f.number_output_features)
+                          if f[i].get_name() in new_feature_names]
                 if len(slices) == f.number_output_features:
                     new_features.append(f)
                 else:
