23835840c3865ec2c63c07c20341fe69f9161ae4
Tamar Grey <tamar.grey@alteryx.com>
2020-09-08 10:51:45 -0400

    v0.19.0.dev updates

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ with open(path.join(dirname, 'README.md')) as f:
     long_description = f.read()
 
 extras_require = {
-  'tsfresh': ['featuretools-tsfresh-primitives >= 0.1.0'],
-  'update_checker': ['featuretools-update-checker >= 1.0.0'],
-  'categorical_encoding': ['categorical-encoding >= 0.2.0'],
-  'nlp_primitives': ['nlp-primitives[complete] >= 1.0.0'],
-  'autonormalize': ['autonormalize >= 1.0.0'],
-  'sklearn_transformer': ['featuretools-sklearn-transformer >= 0.1.1'],
-  'koalas': ['pyspark >= 3.0.0', 'koalas >= 1.1.0']
+    'tsfresh': ['featuretools-tsfresh-primitives >= 0.1.0'],
+    'update_checker': ['featuretools-update-checker >= 1.0.0'],
+    'categorical_encoding': ['categorical-encoding >= 0.2.0'],
+    'nlp_primitives': ['nlp-primitives[complete] >= 1.0.0'],
+    'autonormalize': ['autonormalize >= 1.0.0'],
+    'sklearn_transformer': ['featuretools-sklearn-transformer >= 0.1.1'],
+    'koalas': ['pyspark >= 3.0.0', 'koalas >= 1.1.0']
 }
 extras_require['complete'] = sorted(set(sum(extras_require.values(), [])))
 
 setup(
     name='featuretools',
-    version='0.18.1',
+    version='0.19.0.dev0',
     packages=find_packages(),
     description='a framework for automated feature engineering',
     url='http://featuretools.com',
