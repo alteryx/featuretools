7887e2ec0e0d51ddf364f85fdeab25b956d975b3
tuethan1999 <ethan.tu@alteryx.com>
2020-09-30 18:07:36 -0400

    change version

--- featuretools/tests/test_version.py
+++ featuretools/tests/test_version.py
@@ -2,4 +2,4 @@ from featuretools import __version__
 
 
 def test_version():
-    assert __version__ == "0.19.0"
+    assert __version__ == "0.20.0"
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ extras_require['complete'] = sorted(set(sum(extras_require.values(), [])))
 
 setup(
     name='featuretools',
-    version='0.19.0',
+    version='0.20.0',
     packages=find_packages(),
     description='a framework for automated feature engineering',
     url='http://featuretools.com',
