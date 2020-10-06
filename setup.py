f1f41d0acf277159a1a01988f1d2008e7299dfe3
Gaurav Sheni <gvsheni@gmail.com>
2020-10-06 14:23:02 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
          'Programming Language :: Python :: 3',
          'Programming Language :: Python :: 3.6',
          'Programming Language :: Python :: 3.7'
+         'Programming Language :: Python :: 3.8'
+         'Programming Language :: Python :: 3.9'
     ],
     install_requires=open('requirements.txt').readlines(),
     python_requires='>=3.6, <4',
