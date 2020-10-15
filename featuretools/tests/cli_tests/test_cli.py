6fc393c1b9c897802b3e7414011c2a6d2ddef891
Gaurav Sheni <gvsheni@gmail.com>
2020-10-15 13:18:34 -0400

    fix test

--- featuretools/tests/cli_tests/test_cli.py
+++ featuretools/tests/cli_tests/test_cli.py
@@ -1,15 +1,20 @@
 from click.testing import CliRunner
 
 from featuretools.__main__ import info, list_primitives
+import featuretools as ft
 
 
 def test_info():
     runner = CliRunner()
-    result = runner.invoke(info, [''])
+    result = runner.invoke(info)
     assert result.exit_code == 0
 
 
 def test_list_primitives():
     runner = CliRunner()
-    result = runner.invoke(list_primitives, [''])
+    result = runner.invoke(list_primitives)
     assert result.exit_code == 0
+    for x in ft.list_primitives().columns:
+        assert x in result.output
+    for name in ft.list_primitives()['name']:
+        assert name in result.output
