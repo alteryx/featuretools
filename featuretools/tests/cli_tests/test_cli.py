05bb4e277c0f870a0479ebf0e41a4ef82d63c9e1
Gaurav Sheni <gvsheni@gmail.com>
2020-10-15 12:59:01 -0400

    add assert

--- featuretools/tests/cli_tests/test_cli.py
+++ featuretools/tests/cli_tests/test_cli.py
@@ -5,9 +5,11 @@ from featuretools.__main__ import info, list_primitives
 
 def test_info():
     runner = CliRunner()
-    runner.invoke(info, [''])
+    result = runner.invoke(info, [''])
+    assert result.exit_code == 0
 
 
 def test_list_primitives():
     runner = CliRunner()
-    runner.invoke(list_primitives, [''])
+    result = runner.invoke(list_primitives, [''])
+    assert result.exit_code == 0
