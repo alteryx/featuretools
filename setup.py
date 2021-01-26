879a56cf91f96deae529a419f8bbcfb1f6d858e3
Gaurav Sheni <gvsheni@gmail.com>
2021-01-26 10:36:11 -0500

    skip koalas tests

--- setup.py
+++ setup.py
@@ -31,7 +31,9 @@ setup(
          'Intended Audience :: Developers',
          'Programming Language :: Python :: 3',
          'Programming Language :: Python :: 3.6',
-         'Programming Language :: Python :: 3.7'
+         'Programming Language :: Python :: 3.7',
+         'Programming Language :: Python :: 3.8',
+         'Programming Language :: Python :: 3.9'
     ],
     install_requires=open('requirements.txt').readlines(),
     python_requires='>=3.6, <4',
