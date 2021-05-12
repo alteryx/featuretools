ea6572e9c6690ed99ad3a938242dd797aae926b7
Gaurav Sheni <gvsheni@gmail.com>
2021-05-12 18:03:04 -0400

    remove click

--- featuretools/tests/cli_tests/test_cli.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-def test_info():
-    subprocess.check_output(['featuretools', 'info'])
-
-
-def test_list_primitives():
-    subprocess.check_output(['featuretools', 'list-primitives'])
