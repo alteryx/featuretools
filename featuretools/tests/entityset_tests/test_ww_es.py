b0e21ebbc52dce2775d8fc31f958a518e7b92097
Tamar Grey <tamar.grey@alteryx.com>
2021-06-02 17:21:42 -0400

    add broken test

--- featuretools/tests/entityset_tests/test_ww_es.py
+++ featuretools/tests/entityset_tests/test_ww_es.py
@@ -297,6 +297,17 @@ def test_add_last_time_index(es):
     assert isinstance(es['products'].ww.logical_types['last_time'], Datetime)
 
 
+def test_add_last_time_non_numeric_index(pd_es, ks_es, dask_es):
+    # --> might also be the case any non pandas non numeric bc it both cant replace and also won't match
+    # koalas and dask don't set the underlying index to match the woodwork index
+    pd_es.add_last_time_indexes(['products'])
+    dask_es.add_last_time_indexes(['products'])
+    ks_es.add_last_time_indexes(['products'])
+
+    assert list(to_pandas(pd_es['products']['last_time'], sort_index=True)) == list(to_pandas(dask_es['products']['last_time'], sort_index=True))
+    assert list(to_pandas(pd_es['products']['last_time'], sort_index=True)) == list(to_pandas(ks_es['products']['last_time'], sort_index=True))
+
+
 # --> add a test where there's already a last_time column and it gets replaced
 # --> add a test where we deep copy a ww dataframe with lti
 # --> test lti logical types matching time type
