55eabd800d5082ac8bf4aa5f97696c5288631869
Nate Parsons <nate.parsons@alteryx.com>
2021-08-04 15:29:48 -0500

    fix logical type comparisons

--- featuretools/feature_base/utils.py
+++ featuretools/feature_base/utils.py
@@ -1,5 +1,5 @@
 def is_valid_input(candidate, template):
-    if template.logical_type is not None and candidate.logical_type != template.logical_type:
+    if template.logical_type is not None and not isinstance(candidate.logical_type, type(template.logical_type)):
         return False
     if template.semantic_tags - candidate.semantic_tags != set():
         return False
