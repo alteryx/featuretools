37088d7ff1ae541ac1c0cd70cb203fe74059a8de
Nate Parsons <nate.parsons@alteryx.com>
2021-09-17 12:26:46 -0500

    update version number

--- featuretools/tests/test_version.py
+++ featuretools/tests/test_version.py
@@ -2,4 +2,4 @@ from featuretools import __version__
 
 
 def test_version():
-    assert __version__ == "1.0.0.dev0"
+    assert __version__ == "1.0.0rc1.dev0"
--- featuretools/version.py
+++ featuretools/version.py
@@ -1,1 +1,1 @@
-__version__ = '1.0.0.dev0'
+__version__ = '1.0.0rc1.dev0'
