947c25ede5abc666bd71c82f107adcfe74fe3f17
bchen1116 <bchen1116@gmail.com>
2021-09-23 15:33:50 -0400

    standardize fixed

--- docs/notebook_cleaner.py
+++ docs/notebook_cleaner.py
@@ -20,15 +20,16 @@ def _get_ipython_notebooks(docs_source):
 def _check_delete_empty_cell(notebook, delete=True):
     with open(notebook, "r") as f:
         source = json.load(f)
-        cell = source["cells"][-1]
-        if cell["cell_type"] == "code" and cell["source"] == []:
-            # this is an empty cell, which we should delete
-            if delete:
-                source["cells"] = source["cells"][:-1]
-            else:
-                return False
+    cell = source["cells"][-1]
+    if cell["cell_type"] == "code" and cell["source"] == []:
+        # this is an empty cell, which we should delete
+        if delete:
+            source["cells"] = source["cells"][:-1]
+        else:
+            return False
     if delete:
-        json.dump(source, open(notebook, "w"), ensure_ascii=False, indent=1)
+        with open(notebook, "w") as f:
+            json.dump(source, f, ensure_ascii=False, indent=1)
     else:
         return True
 
@@ -36,38 +37,34 @@ def _check_delete_empty_cell(notebook, delete=True):
 def _check_execution_and_output(notebook):
     with open(notebook, "r") as f:
         source = json.load(f)
-        for cells in source["cells"]:
-            if cells["cell_type"] == "code" and (cells["execution_count"] is not None or cells['outputs']!= []):
-                return False
+    for cells in source["cells"]:
+        if cells["cell_type"] == "code" and (cells["execution_count"] is not None or cells['outputs']!= []):
+            return False
     return True
 
 
 def _fix_execution_and_output(notebook):
     with open(notebook, "r") as f:
         source = json.load(f)
-        for cells in source["cells"]:
-            if cells["cell_type"] == "code" and cells["execution_count"] is not None:
-                cells["execution_count"] = None
-                cells["outputs"] = []
-        source["metadata"]["kernelspec"]["display_name"] = "Python 3"
-        source["metadata"]["kernelspec"]["name"] = "python3"
-    json.dump(source, open(notebook, "w"), ensure_ascii=False, indent=1)
+    for cells in source["cells"]:
+        if cells["cell_type"] == "code" and cells["execution_count"] is not None:
+            cells["execution_count"] = None
+            cells["outputs"] = []
+    source["metadata"]["kernelspec"]["display_name"] = "Python 3"
+    source["metadata"]["kernelspec"]["name"] = "python3"
+    with open(notebook, "w") as f:
+        json.dump(source, f, ensure_ascii=False, indent=1)
 
 
-def _get_notebooks_with_executions(notebooks):
+def _get_notebooks_with_executions_and_empty(notebooks):
     executed = []
+    empty_last_cell = []
     for notebook in notebooks:
         if not _check_execution_and_output(notebook):
             executed.append(notebook)
-    return executed
-
-
-def _get_notebook_with_empty_last_cell(notebooks):
-    empty_last_cell = []
-    for notebook in notebooks:
         if not _check_delete_empty_cell(notebook, delete=False):
             empty_last_cell.append(notebook)
-    return empty_last_cell
+    return (executed, empty_last_cell)
 
 
 def _remove_notebook_empty_last_cell(notebooks):
@@ -88,8 +85,7 @@ def cli():
 @cli.command()
 def standardize():
     notebooks = _get_ipython_notebooks(DOCS_PATH)
-    executed_notebooks = _get_notebooks_with_executions(notebooks)
-    empty_cells = _get_notebook_with_empty_last_cell(notebooks)
+    executed_notebooks, empty_cells = _get_notebooks_with_executions_and_empty(notebooks)
     if executed_notebooks:
         _standardize_outputs(executed_notebooks)
         executed_notebooks = ["\t" + notebook for notebook in executed_notebooks]
@@ -109,8 +105,7 @@ def standardize():
 @cli.command()
 def check_execution():
     notebooks = _get_ipython_notebooks(DOCS_PATH)
-    executed_notebooks = _get_notebooks_with_executions(notebooks)
-    empty_cells = _get_notebook_with_empty_last_cell(notebooks)
+    executed_notebooks, empty_cells = _get_notebooks_with_executions_and_empty(notebooks)
     if executed_notebooks:
         executed_notebooks = ["\t" + notebook for notebook in executed_notebooks]
         executed_notebooks = "\n".join(executed_notebooks)
@@ -121,7 +116,7 @@ def check_execution():
     if empty_cells:
         empty_cells = ["\t" + notebook for notebook in empty_cells]
         empty_cells = "\n".join(empty_cells)
-        click.echo(
+        raise SystemExit(
             f"The following notebooks have empty cells at the end:\n {empty_cells}\n"
             "Please run make lint-fix to fix this."
         )
