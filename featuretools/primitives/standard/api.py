4f037d6b54a71d0fd4c6308be194ba7f6cf980ee
Tamar Grey <tamar.grey@alteryx.com>
2021-11-04 12:53:32 -0400

    Add new primitives to direct import and api reference

--- featuretools/primitives/standard/api.py
+++ featuretools/primitives/standard/api.py
@@ -3,3 +3,4 @@ from .aggregation_primitives import *
 from .binary_transform import *
 from .cum_transform_feature import *
 from .transform_primitive import *
+from .rolling_transform_primitive import *
