81f4ccf43e529dcb231eab50684b5d8fbfab856c
Jacob Boney <jacobaboney@gmail.com>
2022-01-22 10:58:44 -0800

    Update Main

--- featuretools/primitives/standard/api.py
+++ featuretools/primitives/standard/api.py
@@ -4,4 +4,4 @@ from .binary_transform import *
 from .cum_transform_feature import *
 from .rolling_transform_primitive import *
 from .transform_primitive import *
-from .lat_long_transform import *
+from .lat_long_primitive import *
