f946f975e3214625a823b0c73183f7deaa617771
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2022-02-10 13:01:51 -0600

    update test case

--- featuretools/tests/entry_point_tests/test_primitives.py
+++ featuretools/tests/entry_point_tests/test_primitives.py
@@ -7,10 +7,14 @@ from featuretools.tests.entry_point_tests.utils import (
 
 def test_entry_point():
     install_featuretools_primitives()
-    process = import_featuretools('debug')
+    subprocess = import_featuretools('debug')
     uninstall_featuretools_primitives()
+    log = subprocess.stdout.decode()
 
-    expected = 'Ignoring primitive "Sum" from "featuretools_primitives.existing_primitive" because it '
-    expected += 'already exists in "featuretools.primitives.standard.aggregation_primitives"'
-    actual = process.stdout.decode()
-    assert expected in actual
+    invalid_primitive = 'Featuretools failed to load "invalid" primitives from "featuretools_primitives.invalid_primitive". '
+    invalid_primitive += 'For a full stack trace, set logging to debug.'
+    assert invalid_primitive in log
+
+    existing_primitive = 'Ignoring primitive "Sum" from "featuretools_primitives.existing_primitive" because it '
+    existing_primitive += 'already exists in "featuretools.primitives.standard.aggregation_primitives"'
+    assert existing_primitive in log
