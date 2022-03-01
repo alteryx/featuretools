199bb9f8fc09c4b1b58555a22f2919fb17208c20
Dave Reed <dave.reed@alteryx.com>
2022-03-01 15:41:14 -0500

    fixes koalas unpickling error

--- featuretools/entityset/entityset.py
+++ featuretools/entityset/entityset.py
@@ -1158,6 +1158,10 @@ class EntitySet(object):
         for df_name, df in state.get('dataframe_dict', {}).items():
             if ww_schemas[df_name] is not None:
                 df.ww.init(schema=ww_schemas[df_name], validate=False)
+
+            if ks and isinstance(df, ks.DataFrame):
+                self._normalize_dataframe(df)
+
         self.__dict__.update(state)
 
     # ###########################################################################
