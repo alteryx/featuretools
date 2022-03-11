ab6c122b7961980ac6cff412d74543daed1439e5
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2022-03-11 11:16:55 -0600

    check spark import

--- featuretools/entityset/entityset.py
+++ featuretools/entityset/entityset.py
@@ -1579,7 +1579,7 @@ def _get_or_create_index(index, make_index, df):
 
 
 def _create_index(df, index):
-    if isinstance(df, (dd.DataFrame, ps.DataFrame)):
+    if isinstance(df, dd.DataFrame) or (ps and isinstance(df, ps.DataFrame)):
         df[index] = 1
         df[index] = df[index].cumsum() - 1
     else:
