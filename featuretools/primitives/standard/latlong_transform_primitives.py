fc07575b3bbb0d4ce61a2f2b910260680c283021
Dave Reed <dave.reed@alteryx.com>
2022-03-31 12:15:12 -0400

    intial commit

--- featuretools/primitives/standard/latlong_transform_primitives.py
+++ featuretools/primitives/standard/latlong_transform_primitives.py
@@ -81,8 +81,12 @@ class GeoMidpoint(TransformPrimitive):
 
     def get_function(self):
         def geomidpoint_func(latlong_1, latlong_2):
-            lat_1s, lon_1s = _deconstrct_latlongs(latlong_1)
-            lat_2s, lon_2s = _deconstrct_latlongs(latlong_2)
+            lat_1s = np.array([x[0] for x in latlong_1])
+            lon_1s = np.array([x[1] for x in latlong_1])
+
+            lat_2s = np.array([x[0] for x in latlong_2])
+            lon_2s = np.array([x[1] for x in latlong_2])
+
             lat_middle = np.array([lat_1s, lat_2s]).transpose().mean(axis=1)
             lon_middle = np.array([lon_1s, lon_2s]).transpose().mean(axis=1)
             return list(zip(lat_middle, lon_middle))
