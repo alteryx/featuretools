0e3454e74b555c0c890d75f9595f074701329574
Nate Parsons <nate.parsons@alteryx.com>
2022-04-20 10:00:50 -0500

    fix test_boolean_multiply

--- featuretools/tests/primitive_tests/test_transform_features.py
+++ featuretools/tests/primitive_tests/test_transform_features.py
@@ -693,8 +693,6 @@ def test_boolean_multiply(boolean_mult_es):
     es = boolean_mult_es
     to_test = [
         ("numeric", "numeric"),
-        ("numeric", "bool"),
-        ("bool", "numeric"),
         ("bool", "bool"),
     ]
     features = []
