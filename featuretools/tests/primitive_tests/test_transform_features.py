482dec4b3837a49e4c29213491750b2ac646e17c
sbadithe <slyokapi@gmail.com>
2022-06-15 16:23:49 -0700

    Removing extra comment.

--- featuretools/tests/primitive_tests/test_transform_features.py
+++ featuretools/tests/primitive_tests/test_transform_features.py
@@ -99,7 +99,6 @@ def test_init_and_name(es):
         ]
 
     for transform_prim in trans_primitives:
-        # print(f"T: {transform_prim}")
         # skip automated testing if a few special cases
         features_to_use = log_features
         if transform_prim in [NotEqual, Equal]:
