d39c8d4742fe9dd3e73886aec4411c2ad74c4400
Gaurav Sheni <gaurav.sheni@alteryx.com>
2022-07-11 17:23:24 -0400

    run on all files

--- docs/source/setup.py
+++ docs/source/setup.py
@@ -2,18 +2,32 @@ import os
 
 import featuretools as ft
 
+
 def load_feature_plots():
     es = ft.demo.load_mock_customer(return_entityset=True)
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'getting_started/graphs/')
-    agg_feat = ft.AggregationFeature(ft.IdentityFeature(es['sessions'].ww['session_id']),
-                                     'customers', ft.primitives.Count)
-    trans_feat = ft.TransformFeature(ft.IdentityFeature(es['customers'].ww['join_date']), ft.primitives.TimeSincePrevious)
-    demo_feat = ft.AggregationFeature(ft.TransformFeature(ft.IdentityFeature(es['transactions'].ww['transaction_time']),
-                                                          ft.primitives.Weekday),
-                                      'sessions', ft.primitives.Mode)
-    ft.graph_feature(agg_feat, to_file=os.path.join(path, 'agg_feat.dot'))
-    ft.graph_feature(trans_feat, to_file=os.path.join(path, 'trans_feat.dot'))
-    ft.graph_feature(demo_feat, to_file=os.path.join(path, 'demo_feat.dot'))
+    path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "getting_started/graphs/"
+    )
+    agg_feat = ft.AggregationFeature(
+        ft.IdentityFeature(es["sessions"].ww["session_id"]),
+        "customers",
+        ft.primitives.Count,
+    )
+    trans_feat = ft.TransformFeature(
+        ft.IdentityFeature(es["customers"].ww["join_date"]),
+        ft.primitives.TimeSincePrevious,
+    )
+    demo_feat = ft.AggregationFeature(
+        ft.TransformFeature(
+            ft.IdentityFeature(es["transactions"].ww["transaction_time"]),
+            ft.primitives.Weekday,
+        ),
+        "sessions",
+        ft.primitives.Mode,
+    )
+    ft.graph_feature(agg_feat, to_file=os.path.join(path, "agg_feat.dot"))
+    ft.graph_feature(trans_feat, to_file=os.path.join(path, "trans_feat.dot"))
+    ft.graph_feature(demo_feat, to_file=os.path.join(path, "demo_feat.dot"))
 
 
 if __name__ == "__main__":
