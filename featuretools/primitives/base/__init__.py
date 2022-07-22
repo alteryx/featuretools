46f7de160ab8c054a10112976aecc0c36c1b3a6c
Dave Reed <dave.reed@alteryx.com>
2022-07-22 08:17:35 -0400

    added deprecation warning

---
+++ featuretools/primitives/base/__init__.py
@@ -0,0 +1,11 @@
+from featuretools.primitives.core import (
+    AggregationPrimitive,
+    TransformPrimitive,
+    PrimitiveBase,
+)
+from warnings import warn
+
+warn(
+    "featuretools.base module will become deprecated. Use featuretools.core instead",
+    DeprecationWarning,
+)
