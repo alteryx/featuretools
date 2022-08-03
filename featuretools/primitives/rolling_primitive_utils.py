a7c29c7efaad69b2b102b131e1d8d7963048e399
Ozzie <odembowski@alteryx.com>
2022-08-03 14:25:46 -0700

    lint fix, doc string changes and add typing

--- featuretools/primitives/rolling_primitive_utils.py
+++ featuretools/primitives/rolling_primitive_utils.py
@@ -1,9 +1,19 @@
+from typing import TYPE_CHECKING, Callable, Union
+
 import numpy as np
 from pandas import Series
 from pandas.tseries.frequencies import to_offset
 
+if TYPE_CHECKING:
+    from pandas.core.window.rolling import Rolling
 
-def roll_series_with_gap(series, window_size, gap, min_periods):
+
+def roll_series_with_gap(
+    series: Series,
+    window_size: Union[int, str],
+    gap: Union[int, str],
+    min_periods: int,
+) -> Rolling:
     """Provide rolling window calculations where the windows are determined using both a gap parameter
     that indicates the amount of time between each instance and its window and a window length parameter
     that determines the amount of data in each window.
@@ -32,32 +42,6 @@ def roll_series_with_gap(series, window_size, gap, min_periods):
 
     Returns:
         pandas.core.window.rolling.Rolling: The Rolling object for the series passed in.
-
-    Note:
-        Certain operations, like `pandas.core.window.rolling.Rolling.count` that can be performed
-        on the Rolling object returned here may treat NaNs as periods to include in window calculations.
-        So a window [NaN, 1, 3]  when `min_periods=3` will proceed with count, saying there are three periods
-        but only two values and would return count=2. The calculation `max` on the other hand,
-        would say that there are not three periods in that window and would return max=NaN.
-        Most rolling calculations act this way. The implication of that here is that in order to
-        achieve the gap, we insert NaNs at the beinning of the series, which would cause `count` to calculate
-        on windows that technically should not have the correct number of periods. In the RollingCount primitive,
-        we handle this case manually, replacing those values with NaNs. Any primitive that uses this function
-        should determine whether this kind of handling is also necessary.
-
-    Note:
-        Only offset aliases with fixed frequencies can be used when defining gap and window_length.
-        This means that aliases such as `M` or `W` cannot be used, as they can indicate different
-        numbers of days. ('M', because different months are different numbers of days;
-        'W' because week will indicate a certain day of the week, like W-Wed, so that will
-        indicate a different number of days depending on the anchoring date.)
-
-    Note:
-        When using an offset alias to define `gap`, an offset alias must also be used to define `window_size`.
-        This limitation does not exist when using an offset alias to define `window_size`. In fact,
-        if the data has a uniform sampling frequency, it is preferable to use a numeric `gap` as it is more
-        efficient.
-
     """
     _check_window_size(window_size)
     _check_gap(window_size, gap)
@@ -81,7 +65,7 @@ def roll_series_with_gap(series, window_size, gap, min_periods):
     return series.rolling(functional_window_length, min_periods)
 
 
-def _get_rolled_series_without_gap(window, gap_offset):
+def _get_rolled_series_without_gap(window: Series, gap_offset: str) -> Series:
     """Applies the gap offset_string to the rolled window, returning a window
     that is the correct length of time away from the original instance.
 
@@ -109,7 +93,12 @@ def _get_rolled_series_without_gap(window, gap_offset):
     return window[window.index <= gap_bound]
 
 
-def apply_roll_with_offset_gap(window, gap_offset, reducer_fn, min_periods):
+def apply_roll_with_offset_gap(
+    window: Series,
+    gap_offset: str,
+    reducer_fn: Callable[[Series], float],
+    min_periods: int,
+) -> Union[np.nan, float]:
     """Takes in a series to which an offset gap will be applied, removing however many
     rows fall under the gap before applying the reducing function.
 
@@ -136,7 +125,7 @@ def apply_roll_with_offset_gap(window, gap_offset, reducer_fn, min_periods):
     return reducer_fn(window)
 
 
-def _check_window_size(window_size):
+def _check_window_size(window_size: Union[int, str]) -> None:
     # Window length must either be a valid offset alias
     if isinstance(window_size, str):
         try:
@@ -153,7 +142,7 @@ def _check_window_size(window_size):
         raise TypeError("Window length must be either an offset string or an integer.")
 
 
-def _check_gap(window_size, gap):
+def _check_gap(window_size: Union[int, str], gap: Union[int, str]) -> None:
     # Gap must either be a valid offset string that also has an offset string window length
     if isinstance(gap, str):
         if not isinstance(window_size, str):
@@ -177,13 +166,66 @@ def _check_gap(window_size, gap):
 
 
 def apply_rolling_agg_to_series(
-    series,
-    agg_func,
-    window_size,
-    gap=0,
-    min_periods=1,
-    ignore_window_nans=False,
-):
+    series: Series,
+    agg_func: Callable[[Series], float],
+    window_size: Union[int, str],
+    gap: Union[int, str] = 0,
+    min_periods: int = 1,
+    ignore_window_nans: bool = False,
+) -> np.ndarray:
+    """Applies a given aggregation function to a rolled series.
+
+    Args:
+        series (Series): The series over which rolling windows will be created. Must be numeric in nature
+            and have a DatetimeIndex.
+        agg_func (callable[Series -> float]): The aggregation function to apply to a rolled series.
+        window_size (int, string): Specifies the amount of data included in each window.
+            If an integer is provided, will correspond to a number of rows. For data with a uniform sampling frequency,
+            for example of one day, the window_length will correspond to a period of time, in this case,
+            7 days for a window_length of 7.
+            If a string is provided, it must be one of pandas' offset alias strings ('1D', '1H', etc),
+            and it will indicate a length of time that each window should span.
+            The list of available offset aliases, can be found at
+            https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases
+        gap (int, string, optional): Specifies a gap backwards from each instance before the
+            window of usable data begins. If an integer is provided, will correspond to a number of rows.
+            If a string is provided, it must be one of pandas' offset alias strings ('1D', '1H', etc),
+            and it will indicate a length of time between a target instance and the beginning of its window.
+            Defaults to 0, which will include the target instance in the window.
+        min_periods (int, optional): Minimum number of observations required for performing calculations
+            over the window. Can only be as large as window_length when window_length is an integer.
+            When window_length is an offset alias string, this limitation does not exist, but care should be taken
+            to not choose a min_periods that will always be larger than the number of observations in a window.
+            Defaults to 1.
+        ignore_window_nans (bool, optional): Whether or not NaNs in the rolling window should be included in the rolling calculation.
+            NaNs by default get counted towards min_periods. When set to true, all partial values calculated by `agg_func` in the rolling window get replaced with NaN.
+
+    Returns:
+        numpy.ndarray: The array of rolling calculated values.
+
+    Note:
+        Certain operations, like `pandas.core.window.rolling.Rolling.count` that can be performed
+        on the Rolling object returned here may treat NaNs as periods to include in window calculations.
+        So a window [NaN, 1, 3]  when `min_periods=3` will proceed with count, saying there are three periods
+        but only two values and would return count=2. The calculation `max` on the other hand,
+        would say that there are not three periods in that window and would return max=NaN.
+        Most rolling calculations act this way. The implication of that here is that in order to
+        achieve the gap, we insert NaNs at the beinning of the series, which would cause `count` to calculate
+        on windows that technically should not have the correct number of periods. Any primitive that uses this function
+        should determine whether `ignore_window_nans` should be set to `true`.
+
+    Note:
+        Only offset aliases with fixed frequencies can be used when defining gap and window_length.
+        This means that aliases such as `M` or `W` cannot be used, as they can indicate different
+        numbers of days. ('M', because different months are different numbers of days;
+        'W' because week will indicate a certain day of the week, like W-Wed, so that will
+        indicate a different number of days depending on the anchoring date.)
+
+    Note:
+        When using an offset alias to define `gap`, an offset alias must also be used to define `window_size`.
+        This limitation does not exist when using an offset alias to define `window_size`. In fact,
+        if the data has a uniform sampling frequency, it is preferable to use a numeric `gap` as it is more
+        efficient."""
     rolled_series = roll_series_with_gap(series, window_size, gap, min_periods)
     if isinstance(gap, str):
         additional_args = (gap, agg_func, min_periods)
@@ -193,9 +235,6 @@ def apply_rolling_agg_to_series(
         ).values
     applied_rolled_series = rolled_series.apply(agg_func)
 
-    # The shift made to account for gap adds NaNs to the rolled series
-    # Those values get counted towards min_periods when they shouldn't.
-    # So we need to replace any of those partial values with NaNs
     if ignore_window_nans:
         if not min_periods:
             # when min periods is 0 or None it's treated the same as if it's 1
