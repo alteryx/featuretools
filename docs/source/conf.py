f3c14517ed875ec5e2a8c64f9d584da79685e4ba
Gaurav Sheni <gaurav.sheni@alteryx.com>
2022-08-05 15:53:41 -0400

    add theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -154,6 +154,8 @@ html_theme = "pydata_sphinx_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
+    "pygment_light_style": "tango",
+    "pygment_dark_style": "native",
     "icon_links": [
         {
             "name": "GitHub",
