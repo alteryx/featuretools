cb501a4628d3ed1b9ce33cc82d42aa3126ab2f43
Gaurav Sheni <gaurav.sheni@alteryx.com>
2022-08-05 15:39:36 -0400

    fix lang

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -103,7 +103,7 @@ release = featuretools.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = "end"
+language = "en"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
