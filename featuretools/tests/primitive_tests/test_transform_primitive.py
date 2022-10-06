06fc5c5d07398d10a42d14fb9fef85ba409bf063
Gaurav Sheni <gaurav.sheni@alteryx.com>
2022-10-06 16:06:38 -0400

    fix instance

--- featuretools/tests/primitive_tests/test_transform_primitive.py
+++ featuretools/tests/primitive_tests/test_transform_primitive.py
@@ -863,7 +863,7 @@ def test_lag_with_different_dtypes(input_array, expected_output):
 
 
 def test_datetime_tz_primitive():
-    primitive_func = DateToTimeZone.get_function()
+    primitive_func = DateToTimeZone().get_function()
     x = pd.Series(
         [
             datetime(2010, 1, 1, tzinfo=timezone("America/Los_Angeles")),
@@ -877,7 +877,7 @@ def test_datetime_tz_primitive():
 
 
 def test_datetime64():
-    primitive_func = DateToTimeZone.get_function()
+    primitive_func = DateToTimeZone().get_function()
     x = pd.Series(
         [
             datetime(2010, 1, 1),
@@ -891,7 +891,7 @@ def test_datetime64():
 
 
 def test_naive_dates():
-    primitive_func = DateToTimeZone.get_function()
+    primitive_func = DateToTimeZone().get_function()
     x = pd.Series(
         [
             datetime(2010, 1, 1, tzinfo=timezone("America/Los_Angeles")),
@@ -904,7 +904,7 @@ def test_naive_dates():
 
 
 def test_nan():
-    primitive_func = DateToTimeZone.get_function()
+    primitive_func = DateToTimeZone().get_function()
     x = pd.Series(
         [
             datetime(2010, 1, 1, tzinfo=timezone("America/Los_Angeles")),
