5c528afbd66b7726cf3fa725e04359714822faf7
Nate Parsons <nate.parsons@alteryx.com>
2022-10-24 10:28:49 -0500

    update exponential primitives

--- featuretools/primitives/standard/__init__.py
+++ featuretools/primitives/standard/__init__.py
@@ -2,7 +2,6 @@
 from featuretools.primitives.base.aggregation_primitive_base import AggregationPrimitive
 from featuretools.primitives.standard.aggregation_primitives import *
 from featuretools.primitives.standard.datetime_transform_primitives import *
-from featuretools.primitives.standard.exponential_transform_primitives import *
 from featuretools.primitives.standard.latlong_transform_primitives import *
 from featuretools.primitives.standard.rolling_transform_primitives import *
 from featuretools.primitives.standard.transform_primitive import *
--- featuretools/primitives/standard/transform_primitives/__init__.py
+++ featuretools/primitives/standard/transform_primitives/__init__.py
@@ -1,4 +1,5 @@
 # flake8: noqa
 from featuretools.primitives.standard.transform_primitives.binary_transform_primitives import *
 from featuretools.primitives.standard.transform_primitives.cumulative_transform_primitives import *
+from featuretools.primitives.standard.transform_primitives.exponential_transform_primitives import *
 from featuretools.primitives.standard.transform_primitives.natural_language_primitives import *
