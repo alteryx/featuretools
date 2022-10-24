63d2b93b6eb4d09342b028ad4c9be1e56158e9ee
Nate Parsons <nate.parsons@alteryx.com>
2022-10-24 15:12:14 -0500

    fix imports

--- featuretools/primitives/standard/__init__.py
+++ featuretools/primitives/standard/__init__.py
@@ -1,4 +1,5 @@
 # flake8: noqa
 from featuretools.primitives.base.aggregation_primitive_base import AggregationPrimitive
+from featuretools.primitives.base.transform_primitive_base import TransformPrimitive
 from featuretools.primitives.standard.aggregation_primitives import *
 from featuretools.primitives.standard.transform_primitives import *
