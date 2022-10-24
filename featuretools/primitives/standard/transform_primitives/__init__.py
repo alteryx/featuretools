2bb97a15e8cc008d7c2f784403d991ff1fd786df
Nate Parsons <nate.parsons@alteryx.com>
2022-10-24 12:08:43 -0500

    update latlong primitives

--- featuretools/primitives/standard/transform_primitives/__init__.py
+++ featuretools/primitives/standard/transform_primitives/__init__.py
@@ -2,4 +2,5 @@
 from featuretools.primitives.standard.transform_primitives.binary_transform_primitives import *
 from featuretools.primitives.standard.transform_primitives.cumulative_transform_primitives import *
 from featuretools.primitives.standard.transform_primitives.exponential_transform_primitives import *
+from featuretools.primitives.standard.transform_primitives.latlong_transform_primitives import *
 from featuretools.primitives.standard.transform_primitives.natural_language_primitives import *
