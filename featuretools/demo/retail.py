f44a4fb348e6382d358b7d3b04f0d60fe94370ac
sbadithe <shripad.badithe@alteryx.com>
2022-10-25 12:21:40 -0700

    Init

--- featuretools/demo/retail.py
+++ featuretools/demo/retail.py
@@ -13,9 +13,9 @@ def load_retail(id="demo_retail_data", nrows=None, return_single_table=False):
     to a unique fake ``customer_name``, dropped duplicates,
     added columns for ``total`` and ``cancelled`` and
     converted amounts from GBP to USD. You can download the modified CSV `in gz compressed (7 MB)
-    <"https://api.featurelabs.com/datasets/online-retail-logs-2018-08-28.csv.gz">`_
+    <https://api.featurelabs.com/datasets/online-retail-logs-2018-08-28.csv.gz>`_
     or `uncompressed (43 MB)
-    <"https://api.featurelabs.com/datasets/online-retail-logs-2018-08-28.csv">`_ formats.
+    <https://api.featurelabs.com/datasets/online-retail-logs-2018-08-28.csv>`_ formats.
 
     Args:
         id (str):  Id to assign to EntitySet.
