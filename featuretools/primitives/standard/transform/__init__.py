15abcd8c38ae9625cfa22443380e0640ae9b388e
Nate Parsons <nate.parsons@alteryx.com>
2022-10-26 13:13:59 -0500

    move email and url primitives

--- featuretools/primitives/standard/transform/__init__.py
+++ featuretools/primitives/standard/transform/__init__.py
@@ -5,13 +5,8 @@ from featuretools.primitives.standard.transform.cosine import Cosine
 from featuretools.primitives.standard.transform.cumulative import *
 from featuretools.primitives.standard.transform.datetime import *
 from featuretools.primitives.standard.transform.diff import Diff
-from featuretools.primitives.standard.transform.email_address_to_domain import (
-    EmailAddressToDomain,
-)
+from featuretools.primitives.standard.transform.email import *
 from featuretools.primitives.standard.transform.exponential import *
-from featuretools.primitives.standard.transform.is_free_email_domain import (
-    IsFreeEmailDomain,
-)
 from featuretools.primitives.standard.transform.is_in import IsIn
 from featuretools.primitives.standard.transform.is_null import IsNull
 from featuretools.primitives.standard.transform.latlong import *
@@ -26,6 +21,4 @@ from featuretools.primitives.standard.transform.sine import Sine
 from featuretools.primitives.standard.transform.square_root import SquareRoot
 from featuretools.primitives.standard.transform.tangent import Tangent
 from featuretools.primitives.standard.transform.time_series import *
-from featuretools.primitives.standard.transform.url_to_domain import URLToDomain
-from featuretools.primitives.standard.transform.url_to_protocol import URLToProtocol
-from featuretools.primitives.standard.transform.url_to_tld import URLToTLD
+from featuretools.primitives.standard.transform.url import *
