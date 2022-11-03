276669796a5b278e1b6fe91aea52aabaf6766579
sbadithe <shripad.badithe@alteryx.com>
2022-11-03 15:30:40 -0700

    Refactor tests

--- featuretools/tests/primitive_tests/test_expanding_primitives.py
+++ featuretools/tests/primitive_tests/test_expanding_primitives.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pytest
 
 from featuretools.primitives.standard.transform.time_series.expanding import (
     ExpandingCount,
@@ -9,25 +10,32 @@ from featuretools.primitives.standard.transform.time_series.expanding import (
     ExpandingTrend,
 )
 
-"""
-Test implementation.
+# @pytest.mark.parametrize(
+#     "min_periods, gap",
+#     [
+#         (5, 2),
+#         (5, 0),
+#     ],
+# )
+# def test_expanding_count(window_series_pd, min_periods, gap):
+#     test = window_series_pd.shift(gap)
+#     primitive_instance = ExpandingCount(min_periods=min_periods, gap=gap).get_function()
+#     actual = primitive_instance(window_series_pd.index)
+#     expected = test.expanding(min_periods=min_periods).count().index
+#     pd.testing.assert_series_equal(pd.Series(actual), expected)
 
-TODO: Parametrize, refactor
-"""
 
-
-def test_expanding_count():
-    times = pd.date_range(start="2022-01-01", end="2023-01-01", periods=50)
-    primitive_instance = ExpandingCount().get_function()
-    actual = primitive_instance(times)
-    expected = pd.Series([i for i in range(0, 50)]).astype("float64")
-    pd.testing.assert_series_equal(pd.Series(actual), expected)
-
-
-def test_expanding_min(window_series_pd):
-    test = window_series_pd.shift(1)
-    primitive_instance = ExpandingMin().get_function()
-    expected = pd.Series(test.expanding(min_periods=1).min().values)
+@pytest.mark.parametrize(
+    "min_periods, gap",
+    [
+        (5, 2),
+        (5, 0),
+    ],
+)
+def test_expanding_min(window_series_pd, min_periods, gap):
+    test = window_series_pd.shift(gap)
+    primitive_instance = ExpandingMin(min_periods=min_periods, gap=gap).get_function()
+    expected = pd.Series(test.expanding(min_periods=min_periods).min().values)
     actual = primitive_instance(
         numeric=window_series_pd,
         datetime=window_series_pd.index,
@@ -35,10 +43,17 @@ def test_expanding_min(window_series_pd):
     pd.testing.assert_series_equal(pd.Series(actual), expected)
 
 
-def test_expanding_max(window_series_pd):
-    test = window_series_pd.shift(1)
-    primitive_instance = ExpandingMax().get_function()
-    expected = pd.Series(test.expanding(min_periods=1).max().values)
+@pytest.mark.parametrize(
+    "min_periods, gap",
+    [
+        (5, 2),
+        (5, 0),
+    ],
+)
+def test_expanding_max(window_series_pd, min_periods, gap):
+    test = window_series_pd.shift(gap)
+    primitive_instance = ExpandingMax(min_periods=min_periods, gap=gap).get_function()
+    expected = pd.Series(test.expanding(min_periods=min_periods).max().values)
     actual = primitive_instance(
         numeric=window_series_pd,
         datetime=window_series_pd.index,
@@ -46,10 +61,23 @@ def test_expanding_max(window_series_pd):
     pd.testing.assert_series_equal(pd.Series(actual), expected)
 
 
-def test_expanding_std(window_series_pd):
-    test = window_series_pd.shift(1)
-    primitive_instance = ExpandingSTD().get_function()
-    expected = pd.Series(test.expanding(min_periods=1).std().values)
+@pytest.mark.parametrize(
+    "min_periods, gap",
+    [
+        (5, 2),
+        (5, 0),
+    ],
+)
+def test_expanding_std(window_series_pd, min_periods, gap):
+    test = window_series_pd.shift(gap)
+    primitive_instance = ExpandingSTD(min_periods=min_periods, gap=gap).get_function()
+    expected = pd.Series(
+        test.expanding(
+            min_periods=min_periods,
+        )
+        .std()
+        .values,
+    )
     actual = primitive_instance(
         numeric=window_series_pd,
         datetime=window_series_pd.index,
@@ -57,10 +85,17 @@ def test_expanding_std(window_series_pd):
     pd.testing.assert_series_equal(pd.Series(actual), expected)
 
 
-def test_expanding_mean(window_series_pd):
-    test = window_series_pd.shift(1)
-    primitive_instance = ExpandingMean().get_function()
-    expected = pd.Series(test.expanding(min_periods=1).mean().values)
+@pytest.mark.parametrize(
+    "min_periods, gap",
+    [
+        (5, 2),
+        (5, 0),
+    ],
+)
+def test_expanding_mean(window_series_pd, min_periods, gap):
+    test = window_series_pd.shift(gap)
+    primitive_instance = ExpandingMean(min_periods=min_periods, gap=gap).get_function()
+    expected = pd.Series(test.expanding(min_periods=min_periods).mean().values)
     actual = primitive_instance(
         numeric=window_series_pd,
         datetime=window_series_pd.index,
