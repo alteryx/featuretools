892dc5ef38b33e62b08f249a741dfac4478004e6
sbadithe <shripad.badithe@alteryx.com>
2022-11-05 18:54:50 -0700

    Fix ColumnSchema for ExpandingSTD

--- featuretools/primitives/standard/transform/time_series/expanding/expanding_std.py
+++ featuretools/primitives/standard/transform/time_series/expanding/expanding_std.py
@@ -70,7 +70,10 @@ class ExpandingSTD(TransformPrimitive):
     """
 
     name = "expanding_std"
-    input_types = [ColumnSchema(logical_type=Datetime, semantic_tags={"time_index"})]
+    input_types = [
+        ColumnSchema(logical_type=Datetime, semantic_tags={"time_index"}),
+        ColumnSchema(semantic_tags={"numeric"}),
+    ]
     return_type = ColumnSchema(logical_type=Double, semantic_tags={"numeric"})
     uses_full_dataframe = True
 
