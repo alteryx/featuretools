4a3753e56778a46e4df920120f660635efdc4f24
sbadithe <shripad.badithe@alteryx.com>
2022-11-07 09:39:00 -0800

    Simplify docstrings

--- featuretools/primitives/standard/transform/time_series/expanding/expanding_trend.py
+++ featuretools/primitives/standard/transform/time_series/expanding/expanding_trend.py
@@ -18,30 +18,11 @@ class ExpandingTrend(TransformPrimitive):
         Input datetimes should be monotonic.
 
     Args:
-        gap (int, string, optional): Specifies a gap backwards from each instance before the
-            usable data begins. If an integer is provided, it will correspond to a number of rows.
-            If a string is provided, it must be one of pandas' offset alias strings ('1D', '1H', etc),
-            and it will indicate a length of time between a target instance and the beginning of its window.
+        gap (int, optional): Specifies a gap backwards from each instance before the
+            usable data begins. Corresponds to number of rows.
             Defaults to 1.
         min_periods (int, optional): Minimum number of observations required for performing calculations
-            over the window. min_periods must always be less than or equal to the length of the series. (TODO: Check? Gap?)
-            When window_length is an offset alias string, this limitation does not exist,
-            but care should be taken to not choose a min_periods that will always be larger than the number of
-            observations in the series.
-            Defaults to 1.
-
-    Note:
-        Only offset aliases with fixed frequencies can be used when defining gap and h.
-        This means that aliases such as `M` or `W` cannot be used, as they can indicate different
-        numbers of days. ('M', because different months have different numbers of days;
-        'W' because week will indicate a certain day of the week, like W-Wed, so that will
-        indicate a different number of days depending on the anchoring date.)
-
-    Note:
-        When using an offset alias to define `gap`, an offset alias must also be used to define `window_length`.
-        This limitation does not exist when using an offset alias to define `window_length`. In fact,
-        if the data has a uniform sampling frequency, it is preferable to use a numeric `gap` as it is more
-        efficient.
+            over the window. Defaults to 1.
 
 
     Examples:
