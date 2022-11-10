908a548303970f2a227a3b9c6af40ef9943293df
sbadithe <shripad.badithe@alteryx.com>
2022-11-10 11:36:50 -0800

    use np.testing instead of pd.testing and don't check dtype

--- featuretools/tests/primitive_tests/test_postal_primitives.py
+++ featuretools/tests/primitive_tests/test_postal_primitives.py
@@ -12,15 +12,17 @@ def test_single_digit_postal_code(postal_code_series_pd):
     prim = OneDigitPostalCode().get_function()
     actual = prim(postal_code_series_pd)
     expected = [
-        str(code)[0] if not pd.isna(code) else code for code in postal_code_series_pd
+        str(code)[0] if not pd.isna(code) else np.nan for code in postal_code_series_pd
     ]
-    pd.testing.assert_series_equal(pd.Series(actual), pd.Series(expected))
+    actual = [i if not pd.isna(i) else np.nan for i in actual.values]
+    np.testing.assert_array_equal(actual, expected)
 
 
 def test_two_digit_postal_code(postal_code_series_pd):
     prim = TwoDigitPostalCode().get_function()
     actual = prim(postal_code_series_pd)
     expected = [
-        str(code)[:2] if not pd.isna(code) else code for code in postal_code_series_pd
+        str(code)[:2] if not pd.isna(code) else np.nan for code in postal_code_series_pd
     ]
-    pd.testing.assert_series_equal(pd.Series(actual), pd.Series(expected))
+    actual = [i if not pd.isna(i) else np.nan for i in actual.values]
+    np.testing.assert_array_equal(actual.values, expected)
