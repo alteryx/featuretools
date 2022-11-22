2a11e86939b7260a72151e2412514b0a524f3bda
sbadithe <shripad.badithe@alteryx.com>
2022-11-22 11:10:16 -0800

    Refactor helper functions into one function

--- featuretools/tests/primitive_tests/test_postal_primitives.py
+++ featuretools/tests/primitive_tests/test_postal_primitives.py
@@ -8,51 +8,37 @@ from featuretools.primitives.standard.transform.postal import (
 )
 
 
-def one_digit_postal_code_test(postal_series):
-    prim = OneDigitPostalCode().get_function()
-    actual = prim(postal_series)
+def generate_actual_and_expected_for_postal_primitive(postal_series, prim):
+    prim_ops = {
+        "one_digit_postal_code": lambda t: str(t)[0] if pd.notna(t) else np.nan,
+        "two_digit_postal_code": lambda t: str(t)[:2] if pd.notna(t) else np.nan,
+    }
+    op = prim_ops[prim.name]
+    actual = prim().get_function()(postal_series)
     if _is_dask_series(postal_series):
         actual = actual.compute()
     if _is_spark_series(postal_series):
-        expected = [
-            str(code)[0] if pd.notna(code) else np.nan
-            for code in postal_series.to_numpy()
-        ]
+        expected = list(map(op, postal_series.to_numpy()))
         actual = [val if pd.notna(val) else np.nan for val in actual.to_numpy()]
     else:
-        expected = [
-            str(code)[0] if pd.notna(code) else np.nan for code in postal_series
-        ]
-        actual = [val if pd.notna(val) else np.nan for val in actual.values]
-    return actual, expected
-
-
-def two_digit_postal_code_test(postal_series):
-    prim = TwoDigitPostalCode().get_function()
-    actual = prim(postal_series)
-    if _is_dask_series(postal_series):
-        actual = actual.compute()
-    if _is_spark_series(postal_series):
-        expected = [
-            str(code)[:2] if pd.notna(code) else np.nan
-            for code in postal_series.to_numpy()
-        ]
-        actual = [val if pd.notna(val) else np.nan for val in actual.to_numpy()]
-    else:
-        expected = [
-            str(code)[:2] if pd.notna(code) else np.nan for code in postal_series
-        ]
+        expected = list(map(op, postal_series))
         actual = [val if pd.notna(val) else np.nan for val in actual.values]
     return actual, expected
 
 
 def test_one_digit_postal_code(postal_code_dataframe):
     for col in postal_code_dataframe:
-        actual, expected = one_digit_postal_code_test(postal_code_dataframe[col])
+        actual, expected = generate_actual_and_expected_for_postal_primitive(
+            postal_code_dataframe[col],
+            OneDigitPostalCode,
+        )
         np.testing.assert_array_equal(actual, expected)
 
 
 def test_two_digit_postal_code(postal_code_dataframe):
     for col in postal_code_dataframe:
-        actual, expected = two_digit_postal_code_test(postal_code_dataframe[col])
+        actual, expected = generate_actual_and_expected_for_postal_primitive(
+            postal_code_dataframe[col],
+            TwoDigitPostalCode,
+        )
         np.testing.assert_array_equal(actual, expected)
