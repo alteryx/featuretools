a5799fbb11cbf4bc52047605f66104dbdffc1558
sbadithe <shripad.badithe@alteryx.com>
2022-11-22 12:36:10 -0800

    Refactor to compare series, and use pd.NA instead of np.nan

--- featuretools/tests/primitive_tests/test_postal_primitives.py
+++ featuretools/tests/primitive_tests/test_postal_primitives.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 from woodwork.accessor_utils import _is_dask_series, _is_spark_series
 
@@ -10,19 +9,20 @@ from featuretools.primitives.standard.transform.postal import (
 
 def generate_actual_and_expected_for_postal_primitive(postal_series, prim):
     prim_ops = {
-        "one_digit_postal_code": lambda t: str(t)[0] if pd.notna(t) else np.nan,
-        "two_digit_postal_code": lambda t: str(t)[:2] if pd.notna(t) else np.nan,
+        "one_digit_postal_code": lambda t: str(t)[0] if pd.notna(t) else t,
+        "two_digit_postal_code": lambda t: str(t)[:2] if pd.notna(t) else t,
     }
     op = prim_ops[prim.name]
     actual = prim().get_function()(postal_series)
+    if _is_spark_series(postal_series):
+        expected = postal_series.apply(op).to_pandas()
+        actual = actual.to_pandas()
+    else:
+        expected = postal_series.apply(op)
+
     if _is_dask_series(postal_series):
         actual = actual.compute()
-    if _is_spark_series(postal_series):
-        expected = list(map(op, postal_series.to_numpy()))
-        actual = [val if pd.notna(val) else np.nan for val in actual.to_numpy()]
-    else:
-        expected = list(map(op, postal_series))
-        actual = [val if pd.notna(val) else np.nan for val in actual.values]
+        expected = expected.compute()
     return actual, expected
 
 
@@ -32,7 +32,7 @@ def test_one_digit_postal_code(postal_code_dataframe):
             postal_code_dataframe[col],
             OneDigitPostalCode,
         )
-        np.testing.assert_array_equal(actual, expected)
+        pd.testing.assert_series_equal(actual, expected)
 
 
 def test_two_digit_postal_code(postal_code_dataframe):
@@ -41,4 +41,4 @@ def test_two_digit_postal_code(postal_code_dataframe):
             postal_code_dataframe[col],
             TwoDigitPostalCode,
         )
-        np.testing.assert_array_equal(actual, expected)
+        pd.testing.assert_series_equal(actual, expected)
