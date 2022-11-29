fa1dcd375bb646e88b922aaf645aa5a3e203bc24
sbadithe <shripad.badithe@alteryx.com>
2022-11-29 09:06:00 -0800

    fix df iteration and lint. rebasing to try and retrigger frozen codecov

--- featuretools/tests/primitive_tests/test_postal_primitives.py
+++ featuretools/tests/primitive_tests/test_postal_primitives.py
@@ -9,15 +9,21 @@ from featuretools.tests.testing_utils.es_utils import to_pandas
 
 def test_one_digit_postal_code(postal_code_dataframe):
     primitive = OneDigitPostalCode().get_function()
-    for _, row in postal_code_dataframe.iterrows():
-        actual = primitive(row)
-        expected = to_pandas(row.apply(lambda t: str(t)[0] if pd.notna(t) else pd.NA))
+    for x in postal_code_dataframe:
+        series = postal_code_dataframe[x]
+        actual = to_pandas(primitive(series))
+        expected = to_pandas(
+            series.apply(lambda t: str(t)[0] if pd.notna(t) else pd.NA),
+        )
         pd.testing.assert_series_equal(actual, expected)
 
 
 def test_two_digit_postal_code(postal_code_dataframe):
     primitive = TwoDigitPostalCode().get_function()
-    for _, row in postal_code_dataframe.iterrows():
-        actual = primitive(row)
-        expected = to_pandas(row.apply(lambda t: str(t)[:2] if pd.notna(t) else pd.NA))
+    for x in postal_code_dataframe:
+        series = postal_code_dataframe[x]
+        actual = to_pandas(primitive(series))
+        expected = to_pandas(
+            series.apply(lambda t: str(t)[:2] if pd.notna(t) else pd.NA),
+        )
         pd.testing.assert_series_equal(actual, expected)
