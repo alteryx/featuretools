4af49a3bf0077685e782823b69bd2baaae876462
sbadithe <shripad.badithe@alteryx.com>
2022-12-08 15:49:15 -0800

    undo accidental sorting of __init__.py

--- featuretools/__init__.py
+++ featuretools/__init__.py
@@ -1,18 +1,17 @@
 # flake8: noqa
-import logging
-import sys
-import traceback
-
-import pkg_resources
-
-import featuretools.demo
-from featuretools import feature_base
-from featuretools import primitives
-from featuretools import selection
-from featuretools import tests
-from featuretools.computational_backends.api import *
+from featuretools.version import __version__
 from featuretools.config_init import config
 from featuretools.entityset.api import *
+from featuretools import primitives
+from featuretools.synthesis.api import *
+from featuretools.primitives import list_primitives, summarize_primitives
+from featuretools.computational_backends.api import *
+from featuretools import tests
+from featuretools.utils.time_utils import *
+from featuretools.utils.utils_info import show_info
+import featuretools.demo
+from featuretools import feature_base
+from featuretools import selection
 from featuretools.feature_base import (
     AggregationFeature,
     DirectFeature,
@@ -26,15 +25,13 @@ from featuretools.feature_base import (
     save_features,
     load_features,
 )
-from featuretools.primitives import list_primitives, summarize_primitives
-from featuretools.synthesis.api import *
-from featuretools.utils.time_utils import *
-from featuretools.utils.utils_info import show_info
-from featuretools.version import (
-    __version__,
-    ENTITYSET_SCHEMA_VERSION,
-    FEATURES_SCHEMA_VERSION,
-)
+
+import logging
+import pkg_resources
+import sys
+import traceback
+import warnings
+from woodwork import list_logical_types, list_semantic_tags
 
 logger = logging.getLogger("featuretools")
 
