40c884d4b947bc6ac6b7f9e1344b0788f17c300f
Shripad Badithe <60528327+sbadithe@users.noreply.github.com>
2022-12-20 13:05:38 -0800

    Update featuretools/tests/primitive_tests/test_transform_primitive.py
    Co-authored-by: Gaurav Sheni <gvsheni@gmail.com>

--- featuretools/tests/primitive_tests/test_transform_primitive.py
+++ featuretools/tests/primitive_tests/test_transform_primitive.py
@@ -711,7 +711,7 @@ def test_email_address_to_domain_empty_string():
 
 def test_email_address_to_domain_empty_series():
     email_address_to_domain = EmailAddressToDomain()
-    array = pd.Series([], dtype="object")
+    array = pd.Series([], dtype="category")
     answers = pd.Series(email_address_to_domain(array))
     correct_answers = pd.Series([], dtype="category")
     pd.testing.assert_series_equal(answers, correct_answers)
