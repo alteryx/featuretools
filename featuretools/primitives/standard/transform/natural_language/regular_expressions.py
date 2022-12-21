2c849b850bfa308efebef8dc7df7d10adee84e0a
sbadithe <shripad.badithe@alteryx.com>
2022-12-20 16:16:22 -0800

    simplify delimiters

--- featuretools/primitives/standard/transform/natural_language/regular_expressions.py
+++ featuretools/primitives/standard/transform/natural_language/regular_expressions.py
@@ -1,7 +1,6 @@
 # Define regular expressions to be used by NatLang primitives
 
 import re
-from string import punctuation
 
-DELIMITERS = re.escape(r"[-.!?]") + " \n\t"
+DELIMITERS = " \n\t"
 DELIMITERS = f"[{DELIMITERS}]"
