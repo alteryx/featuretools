0f3d4f66260f6ee9388f3ce6a4da18a0de7f378b
sbadithe <shripad.badithe@alteryx.com>
2022-12-22 19:58:04 -0800

    don't run on windows

--- featuretools/tests/primitive_tests/natural_language_primitives_tests/test_natural_language_perf.py
+++ featuretools/tests/primitive_tests/natural_language_primitives_tests/test_natural_language_perf.py
@@ -18,43 +18,44 @@ from featuretools.primitives import (
     UpperCaseCount,
     WhitespaceCount,
 )
+from platform import system
 
 TIMEOUT_THRESHOLD = 20
 
-
-@pytest.mark.parametrize(
-    "primitive",
-    [
-        CountString,
-        MeanCharactersPerWord,
-        MedianWordLength,
-        NumUniqueSeparators,
-        TitleWordCount,
-        TotalWordLength,
-        NumberOfHashtags,
-        NumberOfMentions,
-        NumberOfUniqueWords,
-        NumberOfWordsInQuotes,
-        PunctuationCount,
-        UpperCaseCount,
-        WhitespaceCount,
-    ],
-)
-def test_natlang_primitive_does_not_timeout(shuffled_test_strings, primitive):
-    def handle_SIGALRM(signum, frame):
-        raise TimeoutError(
-            f"NaturalLanguage primitive took longer than {TIMEOUT_THRESHOLD}"
-        )
-
-    signal.signal(signal.SIGALRM, handle_SIGALRM)
-    for text in shuffled_test_strings:
-        signal.alarm(TIMEOUT_THRESHOLD)
-        try:
-            primitive().get_function()(pd.Series(text))
-        except TimeoutError:
+if system == "Linux" or system == "Darwin":
+    @pytest.mark.parametrize(
+        "primitive",
+        [
+            CountString,
+            MeanCharactersPerWord,
+            MedianWordLength,
+            NumUniqueSeparators,
+            TitleWordCount,
+            TotalWordLength,
+            NumberOfHashtags,
+            NumberOfMentions,
+            NumberOfUniqueWords,
+            NumberOfWordsInQuotes,
+            PunctuationCount,
+            UpperCaseCount,
+            WhitespaceCount,
+        ],
+    )
+    def test_natlang_primitive_does_not_timeout(shuffled_test_strings, primitive):
+        def handle_SIGALRM(signum, frame):
             raise TimeoutError(
-                f"NaturalLanguage primitive {primitive.name} took longer than {TIMEOUT_THRESHOLD}"
+                f"NaturalLanguage primitive took longer than {TIMEOUT_THRESHOLD}"
             )
-        else:
-            # reset alarm
-            signal.alarm(0)
+
+        signal.signal(signal.SIGALRM, handle_SIGALRM)
+        for text in shuffled_test_strings:
+            signal.alarm(TIMEOUT_THRESHOLD)
+            try:
+                primitive().get_function()(pd.Series(text))
+            except TimeoutError:
+                raise TimeoutError(
+                    f"NaturalLanguage primitive {primitive.name} took longer than {TIMEOUT_THRESHOLD}"
+                )
+            else:
+                # reset alarm
+                signal.alarm(0)
