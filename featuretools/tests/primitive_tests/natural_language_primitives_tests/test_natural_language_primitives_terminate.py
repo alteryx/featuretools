7b5f0680a1bae86a30eaf5f2520f006c45ce17ea
sbadithe <shripad.badithe@alteryx.com>
2023-01-10 15:10:02 -0800

    don't sort .values

--- featuretools/tests/primitive_tests/natural_language_primitives_tests/test_natural_language_primitives_terminate.py
+++ featuretools/tests/primitive_tests/natural_language_primitives_tests/test_natural_language_primitives_terminate.py
@@ -11,7 +11,7 @@ class TestNaturalLanguagePrimitivesTerminate:
     primitives = sorted(_get_natural_language_primitives())
 
     @pytest.mark.timeout(TIMEOUT_THRESHOLD)
-    @pytest.mark.parametrize("primitive", primitives.values())
+    @pytest.mark.parametrize("primitive", [prim for _, prim in primitives])
     def test_natlang_primitive_does_not_timeout(
         self,
         strings_that_have_triggered_errors_before,
