dd1e23a134fb3d3697accd0e852c7f296579dbdb
sbadithe <shripad.badithe@alteryx.com>
2023-01-10 14:44:48 -0800

    debug run, try to fix test failures

--- featuretools/tests/primitive_tests/natural_language_primitives_tests/test_natural_language_primitives_terminate.py
+++ featuretools/tests/primitive_tests/natural_language_primitives_tests/test_natural_language_primitives_terminate.py
@@ -5,12 +5,18 @@ from featuretools.primitives.utils import _get_natural_language_primitives
 
 TIMEOUT_THRESHOLD = 20
 
-primitives = _get_natural_language_primitives()
 
-@pytest.mark.timeout(TIMEOUT_THRESHOLD)
-@pytest.mark.parametrize("primitive", primitives)
-def test_natlang_primitive_does_not_timeout(
-    strings_that_have_triggered_errors_before, primitive
-):
-    for text in strings_that_have_triggered_errors_before:
-        primitive().get_function()(pd.Series(text))
+class TestNaturalLanguagePrimitivesTerminate:
+
+    primitives = _get_natural_language_primitives()
+
+    @pytest.mark.timeout(TIMEOUT_THRESHOLD)
+    @pytest.mark.parametrize("primitive", primitives)
+    def test_natlang_primitive_does_not_timeout(
+        self,
+        strings_that_have_triggered_errors_before,
+        primitive,
+    ):
+        print(f"Primitive {primitive}")
+        for text in strings_that_have_triggered_errors_before:
+            primitive().get_function()(pd.Series(text))
