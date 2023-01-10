529205c78996012f2ad854152ded543911ef7ec4
sbadithe <shripad.badithe@alteryx.com>
2023-01-10 11:40:13 -0800

    debug run, remove skipif

--- featuretools/tests/primitive_tests/natural_language_primitives_tests/test_natural_language_primitives_terminate.py
+++ featuretools/tests/primitive_tests/natural_language_primitives_tests/test_natural_language_primitives_terminate.py
@@ -1,5 +1,3 @@
-from platform import system
-
 import pandas as pd
 import pytest
 
@@ -9,13 +7,7 @@ TIMEOUT_THRESHOLD = 20
 
 primitives = _get_natural_language_primitives()
 
-print(f"System: {system()}")
-
 @pytest.mark.timeout(TIMEOUT_THRESHOLD)
-@pytest.mark.skipif(
-    not (system() == "Linux" or system() == "Darwin"),
-    reason="timeout test only supported on UNIX systems",
-)
 @pytest.mark.parametrize("primitive", primitives)
 def test_natlang_primitive_does_not_timeout(
     strings_that_have_triggered_errors_before, primitive
