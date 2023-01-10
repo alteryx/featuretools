03c6d01def5d3ff7d1965bb9f61be6c7c49a7a77
sbadithe <shripad.badithe@alteryx.com>
2023-01-10 15:06:21 -0800

    add back timeout

--- featuretools/tests/primitive_tests/natural_language_primitives_tests/test_natural_language_primitives_terminate.py
+++ featuretools/tests/primitive_tests/natural_language_primitives_tests/test_natural_language_primitives_terminate.py
@@ -10,12 +10,12 @@ class TestNaturalLanguagePrimitivesTerminate:
 
     primitives = sorted(_get_natural_language_primitives().values())
 
+    @pytest.mark.timeout(TIMEOUT_THRESHOLD)
     @pytest.mark.parametrize("primitive", primitives)
     def test_natlang_primitive_does_not_timeout(
         self,
         strings_that_have_triggered_errors_before,
         primitive,
     ):
-        print(f"Primitive {primitive}")
         for text in strings_that_have_triggered_errors_before:
             primitive().get_function()(pd.Series(text))
