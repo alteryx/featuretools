206a278886dba5b9a7c86e1c20ac793c60718917
Gaurav Sheni <gaurav.sheni@alteryx.com>
2023-01-25 11:27:39 -0500

    fix upper case word

--- featuretools/tests/primitive_tests/test_upper_case_word_count.py
+++ featuretools/tests/primitive_tests/test_upper_case_word_count.py
@@ -18,7 +18,7 @@ class TestUpperCaseWordCount:
             dtype="string",
         )
         primitive_func = self.primitive().get_function()
-        answers = pd.Series([2.0, 1.0, 2.0, 1.0], dtype="float64")
+        answers = pd.Series([1.0, 1.0, 2.0, 1.0], dtype="float64")
         pd.testing.assert_series_equal(primitive_func(x), answers, check_names=False)
 
     def test_nan(self):
@@ -31,5 +31,5 @@ class TestUpperCaseWordCount:
             dtype="string",
         )
         primitive_func = self.primitive().get_function()
-        answers = pd.Series([np.nan, 0.0, 2.0], dtype="float64")
+        answers = pd.Series([np.nan, 0.0, 1.0], dtype="float64")
         pd.testing.assert_series_equal(primitive_func(x), answers, check_names=False)
