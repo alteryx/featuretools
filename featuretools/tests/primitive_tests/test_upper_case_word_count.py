d61c591ac5c73f4f70388f964d7fc0f9682702f1
Shripad Badithe <60528327+sbadithe@users.noreply.github.com>
2023-02-01 07:48:57 -0800

    Update featuretools/tests/primitive_tests/test_upper_case_word_count.py
    Co-authored-by: Gaurav Sheni <gvsheni@gmail.com>

--- featuretools/tests/primitive_tests/test_upper_case_word_count.py
+++ featuretools/tests/primitive_tests/test_upper_case_word_count.py
@@ -19,7 +19,7 @@ class TestUpperCaseWordCount:
             dtype="string",
         )
         primitive_func = self.primitive().get_function()
-        answers = pd.Series([2, 1, 2, 3, 1])
+        answers = pd.Series([2, 1, 2, 3, 1], dtype='Int64')
         pd.testing.assert_series_equal(
             primitive_func(x),
             answers,
