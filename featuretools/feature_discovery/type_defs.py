f36a59453be153cbf300840fdcac9864c4030f61
Dave Reed <dave.reed@alteryx.com>
2023-03-21 15:10:39 -0400

    added serialize and deserialize

--- featuretools/feature_discovery/type_defs.py
+++ featuretools/feature_discovery/type_defs.py
@@ -1,20 +1,53 @@
+from __future__ import annotations
+
 import hashlib
 from dataclasses import dataclass, field
-from typing import List, Optional, Set, Type
+from functools import total_ordering
+from inspect import isclass
+from typing import Dict, List, Optional, Set, Type
 
+import woodwork as ww
 from woodwork.logical_types import LogicalType
 
+import featuretools as ft
+from featuretools.feature_base.feature_base import FeatureBase
 from featuretools.primitives.base.primitive_base import PrimitiveBase
 
 
+def get_all_primitives():
+    """Helper function that selects all primitives
+    that are instances of `primitive_kind`
+    """
+    primitives = set()
+    for attribute_string in dir(ft.primitives):
+        attribute = getattr(ft.primitives, attribute_string)
+        if isclass(attribute):
+            if issubclass(attribute, PrimitiveBase) and attribute.name:
+                primitives.add(attribute)
+    return {prim.__name__: prim for prim in primitives}
+
+
+def get_all_logical_types():
+    """Helper function that selects all primitives
+    that are instances of `primitive_kind`
+    """
+    all_types = ww.type_system.registered_types
+    return {lt.__name__: lt for lt in all_types}
+
+
+primitives_map = get_all_primitives()
+logical_types_map = get_all_logical_types()
+
+
+@total_ordering
 @dataclass
 class Feature:
     name: Optional[str]
 
-    logical_type: Type[LogicalType]
+    logical_type: Optional[Type[LogicalType]] = None
     tags: Set[str] = field(default_factory=set)
     primitive: Optional[Type[PrimitiveBase]] = None
-    base_columns: List[str] = field(default_factory=list)
+    base_features: List[Feature] = field(default_factory=list)
     df_id: Optional[str] = None
     id: str = field(init=False)
 
@@ -22,7 +55,7 @@ class Feature:
     def hash(
         name: Optional[str],
         primitive: Optional[Type[PrimitiveBase]] = None,
-        base_columns: List[str] = [],
+        base_features: List[Feature] = [],
         df_id: Optional[str] = None,
     ):
         hash_msg = hashlib.sha256()
@@ -36,13 +69,13 @@ class Feature:
             commutative = primitive.commutative
             hash_msg.update(primitive_name.encode("utf-8"))
 
-            assert len(base_columns) > 0
-            base_columns = base_columns
+            assert len(base_features) > 0
+            base_columns = base_features
             if commutative:
-                base_columns.sort()
+                base_features.sort()
 
             for c in base_columns:
-                hash_msg.update(c.encode("utf-8"))
+                hash_msg.update(c.id.encode("utf-8"))
 
         else:
             assert name
@@ -50,8 +83,11 @@ class Feature:
 
         return hash_msg.hexdigest()
 
-    def __eq__(self, other):
+    def __eq__(self, other: Feature):
         return self.id == other.id
+
+    def __lt__(self, other: Feature):
+        return self.id < other.id
 
     def __ne__(self, other):
         return self.id != other.id
@@ -63,9 +99,73 @@ class Feature:
         return self.hash(
             name=self.name,
             primitive=self.primitive,
-            base_columns=self.base_columns,
+            base_features=self.base_features,
             df_id=self.df_id,
         )
 
     def __post_init__(self):
         self.id = self._generate_hash()
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "logical_type": self.logical_type.__name__ if self.logical_type else None,
+            "tags": list(self.tags),
+            "primitive": self.primitive.__name__ if self.primitive else None,
+            "base_features": [x.to_dict() for x in self.base_features],
+            "df_id": self.df_id,
+            "id": self.id,
+        }
+
+    @staticmethod
+    def from_dict(input_dict: Dict) -> Feature:
+        base_features = [Feature.from_dict(x) for x in input_dict["base_features"]]
+
+        primitive = (
+            primitives_map[input_dict["primitive"]] if input_dict["primitive"] else None
+        )
+
+        logical_type = logical_types_map[input_dict["logical_type"]]
+
+        hydrated_feature = Feature(
+            name=input_dict["name"],
+            logical_type=logical_type,
+            tags=set(input_dict["tags"]),
+            primitive=primitive,
+            base_features=base_features,
+            df_id=input_dict["df_id"],
+        )
+
+        assert hydrated_feature.id == input_dict["id"]
+
+        return hydrated_feature
+
+
+def convert_old_to_new(feature: FeatureBase):
+    base_features = [convert_old_to_new(x) for x in feature.base_features]
+
+    name = feature.get_name()
+    col_schema = feature.column_schema
+
+    logical_type = col_schema.logical_type
+    if logical_type is not None:
+        assert issubclass(type(logical_type), LogicalType)
+        logical_type = type(logical_type)
+
+    tags = col_schema.semantic_tags
+
+    primitive = type(feature.primitive)
+    if primitive == PrimitiveBase:
+        primitive = None
+
+    f = Feature(
+        name=name,
+        logical_type=logical_type,
+        tags=tags,
+        primitive=primitive,
+        base_features=base_features,
+        # TODO: replace this with dataframe name?
+        df_id=None,
+    )
+
+    return f
--- featuretools/tests/feature_discovery/test_type_defs.py
+++ featuretools/tests/feature_discovery/test_type_defs.py
@@ -1,7 +1,15 @@
+import json
+
 from woodwork.logical_types import Double
 
-from featuretools.feature_discovery.type_defs import Feature
+from featuretools.entityset.entityset import EntitySet
+from featuretools.feature_base.feature_base import FeatureBase
+from featuretools.feature_discovery.type_defs import Feature, convert_old_to_new
 from featuretools.primitives import AddNumeric
+from featuretools.synthesis import dfs
+from featuretools.tests.testing_utils.generate_fake_dataframe import (
+    generate_fake_dataframe,
+)
 
 
 def test_feature_type():
@@ -11,3 +19,104 @@ def test_feature_type():
         primitive=AddNumeric,
         logical_type=Double,
     )
+
+
+def test_feature_to_dict():
+    f1 = Feature("f1", Double)
+    f2 = Feature("f2", Double)
+    f = Feature(
+        name="Column 1",
+        primitive=AddNumeric,
+        logical_type=Double,
+        base_features=[f1, f2],
+    )
+
+    expected = {
+        "name": "Column 1",
+        "logical_type": "Double",
+        "tags": [],
+        "primitive": "AddNumeric",
+        "base_features": [
+            {
+                "name": "f1",
+                "logical_type": "Double",
+                "tags": [],
+                "primitive": None,
+                "base_features": [],
+                "df_id": None,
+                "id": "3f524cdc07a11d7c6220bdb049fe8dd41b27483c96cc59b581e022d547290d69",
+            },
+            {
+                "name": "f2",
+                "logical_type": "Double",
+                "tags": [],
+                "primitive": None,
+                "base_features": [],
+                "df_id": None,
+                "id": "e4ab4e3b1493d5a997b4e51cdefbaa10570ef3ea9432bd72e7b6a89654ceb7f6",
+            },
+        ],
+        "df_id": None,
+        "id": "b999e3fdddba9cfca2d63fe4030332210578f07698f21e82d3c91c094b1862cf",
+    }
+
+    actual = f.to_dict()
+    json_str = json.dumps(actual)
+    assert actual == expected
+    assert json.dumps(expected) == json_str
+
+
+def test_feature_from_dict():
+    f1 = Feature("f1", Double)
+    f2 = Feature("f2", Double)
+    f_orig = Feature(
+        name="Column 1",
+        primitive=AddNumeric,
+        logical_type=Double,
+        base_features=[f1, f2],
+    )
+
+    input_dict = f_orig.to_dict()
+    f_from_dict = Feature.from_dict(input_dict)
+    assert f_orig == f_from_dict
+
+
+def test_convert_old_to_new():
+    col_defs = [
+        ("f_1", "Double"),
+        ("f_2", "Double"),
+    ]
+
+    df = generate_fake_dataframe(
+        col_defs=col_defs,
+        include_index=True,
+    )
+
+    es = EntitySet(id="nums")
+    es.add_dataframe(df, "nums", index="idx")
+
+    fdefs = dfs(
+        entityset=es,
+        target_dataframe_name="nums",
+        trans_primitives=[AddNumeric],
+        features_only=True,
+    )
+    assert isinstance(fdefs, list)
+    assert isinstance(fdefs[0], FeatureBase)
+
+    convert_old_to_new(fdefs[2])
+
+    converted_features = set([convert_old_to_new(x) for x in fdefs])
+
+    f1 = Feature("f_1", Double)
+    f2 = Feature("f_2", Double)
+    f3 = Feature(
+        name="f_1 + f_2",
+        tags={"numeric"},
+        primitive=AddNumeric,
+        base_features=[f1, f2],
+    )
+
+    orig_features = set([f1, f2, f3])
+
+    assert len(orig_features.symmetric_difference(converted_features)) == 0
