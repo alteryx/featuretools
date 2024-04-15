f82757e14a26c2dad74ed66c10247701b5fca608
Nate Parsons <nate.parsons@alteryx.com>
2024-04-15 13:10:30 -0400

    more ci work

--- featuretools/tests/entityset_tests/test_ww_es.py
+++ featuretools/tests/entityset_tests/test_ww_es.py
@@ -654,8 +654,6 @@ def latlong_df():
 
 
 def test_replace_dataframe_data_transformation(latlong_df):
-    dask = pytest.importorskip("dask", reason="Dask not installed, skipping")
-    dask.config.set({"dataframe.convert-string": False})
     initial_df = latlong_df.copy()
     initial_df.ww.init(
         name="latlongs",
