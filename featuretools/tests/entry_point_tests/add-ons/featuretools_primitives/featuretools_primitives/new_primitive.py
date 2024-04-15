dcee907c14c7acdf41b48a905fdca223ecdd330a
Nate Parsons <nate.parsons@alteryx.com>
2024-04-15 11:57:49 -0400

    fix

--- featuretools/tests/entry_point_tests/add-ons/featuretools_primitives/featuretools_primitives/new_primitive.py
+++ featuretools/tests/entry_point_tests/add-ons/featuretools_primitives/featuretools_primitives/new_primitive.py
@@ -4,5 +4,4 @@ from featuretools.primitives.base import TransformPrimitive
 class NewPrimitive(TransformPrimitive):
     """A primitive that should not currently exist for testing."""
 
-    name = "new_primitive"
     pass
