6144e2548d2bedc5edd9d24af629fa475d88e494
arminm22 <113990269+arminm22@users.noreply.github.com>
2024-08-01 22:19:45 +0200

    added gui example

---
+++ featuretools/gui/start.py
@@ -0,0 +1,30 @@
+import wx
+
+class MyPanel(wx.Panel):
+    def __init__(self, parent=None):
+        super(MyPanel, self).__init__(parent)
+
+        # Create a label
+        self.label = wx.StaticText(self, wx.ID_ANY, "This is a label")
+        self.label.SetPosition((50, 20))  # Set position manually
+        
+        # Create a button
+        self.button = wx.Button(self, wx.ID_ANY, "Test Button")
+        self.button.SetPosition((50, 60))  # Set position manually
+        
+        # Bind event handler for the button
+        self.Bind(wx.EVT_BUTTON, self.onButtonClicked, self.button)
+
+    def onButtonClicked(self, event):
+        print("Button clicked!")
+
+class MyApp(wx.App):
+    def OnInit(self):
+        frame = wx.Frame(None, wx.ID_ANY, "Panel Example")
+        panel = MyPanel(frame)
+        frame.Show()
+        return True
+
+if __name__ == "__main__":
+    app = MyApp()
+    app.MainLoop()
