aaf3307d5b71d2ce1a5130f22697d968c8ace450
y9rabbito <sudipta.kumar.biswas@protonmail.com>
2026-04-17 17:49:04 +0530

    Fixed some typos

--- featuretools/primitives/standard/transform/time_series/rolling_mean.py
+++ featuretools/primitives/standard/transform/time_series/rolling_mean.py
@@ -92,11 +92,10 @@ class RollingMean(TransformPrimitive):
         self.min_periods = min_periods
 
     def get_function(self):
-        # def rolling_mean(datetime, numeric):
-            x = pd.Series(numeric.values, index=datetime.values)
+        def rolling_mean(datetime, numeric):
+            # x = pd.Series(numeric.values, index=datetime.values)
             x = pd.Series(numeric.values, index=pd.to_datetime(datetime))
             x = x.sort_index(kind="stable")
-            
             return apply_rolling_agg_to_series(
                 x,
                 np.mean,
